b88f9c134960e18c8161ebea304a5ab1613ba521
rogerlew <rogerlew@gmail.com>
2018-04-04 13:09:54 -0700

    fix #56

---
+++ wepppy/ssurgo/tests/testsoils/1652031.sol
@@ -0,0 +1,58 @@
+7778
+# 
+#             Hydro TBD Utils v.0.1.0 (c) University of Idaho
+# 
+#   Build Date: 2018-04-04 13:07:58.822940
+#   Source Data: Surgo
+# 
+# Mukey: 1652031
+# Major Component: 14518429 (comppct_r = 40.0)
+# 
+#   Chkey   hzname  mask hzdepb_r  ksat_r fraggt10_r frag3to10_r dbthirdbar_r
+# ----------------------------------------------------------------------------
+#  41810106   H1              5.0    28.0        5.0        10.0          1.3
+#  41810107   H2             13.0    28.0        0.0         5.0          1.3
+#  41810105   H3     R       38.0   0.005         -           -           1.4
+# 
+# Restricting Layer:
+#     ksat threshold: 2.00000
+#     type: Lithic bedrock
+#     ksat: 0.00005
+# 
+# defaults applied to missing chorizon data:
+#     sandtotal_r  ->      66.800
+#     claytotal_r  ->       7.000
+#     om_r         ->       7.000
+#     cec7_r       ->      11.300
+#     sandvf_r     ->      10.000
+#     ksat_r       ->      28.000
+#     dbthirdbar_r ->       1.400
+#     smr          ->      55.500
+#     field_cap    ->       0.242
+#     wilt_pt      ->       0.115
+# 
+# Build Notes:
+#   initial assumed ksat = 0.750
+# THIS FILE AND THE CONTAINED DATA IS PROVIDED BY THE UNIVERSITY OF IDAHO 
+# 'AS IS' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED 
+# TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A 
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL UNIVERSITY OF IDAHO 
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHElementTreeHER IN 
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+# ARISING IN ANY WAY OUT OF THE USE OF THIS FILE, EVEN IF ADVISED OF THE 
+# POSSIBILITY OF SUCH DAMAGE.
+# 
+# 
+#   If you change the original contexts of this file please 
+#   indicate it by putting an 'X' in the box here -> [ ]
+# 
+# 
+Any comments:
+1 1
+'Lithnip-Meiss-Hawkinspeak association, 30 to 75 percent slopes'		'GRX-SL'	2	0.2300	0.7500	4764260.00	0.0076	2.9652	0.0000
+	   50.000	    1.30	 100.8000	 10.0	   0.3294	   0.1647	  66.80	  14.00	   1.50	  12.50	  83.00
+	  160.000	    1.30	 100.8000	 10.0	   0.2947	   0.1439	  65.90	  15.00	   0.75	  12.50	  71.50
+1 10000.0 0.00018
