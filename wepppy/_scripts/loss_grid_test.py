17be4ef2dfdd769b6c8623edb364d1a1975f745f
rogerlew <rogerlew@gmail.com>
2018-04-05 11:21:52 -0700

    data acquisition and processing, cleaned up _scripts folder

--- wepppy/_scripts/loss_grid_test.py
+++
@@ -1,7 +0,0 @@
-
-from wepppy.nodb import Wepp
-
-if __name__ == "__main__":
-    wd = '/geodata/weppcloud_runs/bb967f25-9fd6-4641-b737-bb10a1cf7843'
-    wepp = Wepp.getInstance(wd)
-    wepp.make_loss_grid()
