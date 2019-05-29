2654426390a83e813a3bc112a663cc3886c50aa0
Roger Lew <rogerlew@gmail.com>
2019-05-29 10:31:41 -0700

    standalone deployment fix

--- wepppy/weppcloud/standalone_config.py
+++ wepppy/weppcloud/standalone_config.py
@@ -21,3 +21,29 @@ def config_app(app):
     app.config['W3W_API_KEY'] = 'XBL7LV7I'
 
     return app
+
+
+def _init(db, user_datastore):
+    from wepppy.weppcloud import RunStatistics
+    try:
+        rs = RunStatistics('/geodata/weppcloud_runs')
+    except:
+        raise
+
+    db.drop_all()
+    db.create_all()
+    user_datastore.create_role(name='User', description='Regular WeppCloud User')
+    user_datastore.create_role(name='PowerUser', description='WeppCloud PowerUser')
+    user_datastore.create_role(name='Admin', description='WeppCloud Administrator')
+    user_datastore.create_role(name='Dev', description='Developer')
+    user_datastore.create_role(name='Root', description='Root')
+
+    user_datastore.create_user(email='root@weppcloud.com', password='test123',
+                               first_name='Super', last_name='User')
+    user_datastore.add_role_to_user('root@weppcloud.com', 'User')
+    user_datastore.add_role_to_user('root@weppcloud.com', 'PowerUser')
+    user_datastore.add_role_to_user('root@weppcloud.com', 'Admin')
+    user_datastore.add_role_to_user('root@weppcloud.com', 'Dev')
+    user_datastore.add_role_to_user('root@weppcloud.com', 'Root')
+
+    db.session.commit()
