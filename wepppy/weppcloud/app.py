e4b89ced364b00e06a4260493618925f0b55de40
Roger Lew <rogerlew@gmail.com>
2019-05-29 10:25:32 -0700

    standalone deployment fix

--- wepppy/weppcloud/app.py
+++ wepppy/weppcloud/app.py
@@ -239,8 +239,13 @@ security = Security(app, user_datastore,
 @app.before_first_request
 def init_db():
     if not _exists('/geodata/weppcloud_runs/standalone.db') and _hostname != 'wepp1':
-        import app_config
-        app_config._init(db, user_datastore)
+
+        if 'wepp1' in _hostname:
+            from wepppy.weppcloud.app_config import _init
+        else:
+            from wepppy.weppcloud.standalone_config import _init
+
+        _init(db, user_datastore)
 
 
 def get_run_owners(runid):
@@ -2752,4 +2757,4 @@ def runid_query(wc):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=80)
