40de6cfe0e86e2994e93257682c542475efcb6c3
Roger Lew <rogerlew@gmail.com>
2019-06-09 12:48:47 -0700

    au data

---
+++ wepppy/au/soils/ceea8d96d69f8e225092b521951162ab94293ae4bae29380eebb6c43.sol
@@ -0,0 +1,72 @@
+2006.2
+#
+#            WEPPcloud (c) University of Idaho
+#
+#  Build Date: 2019-06-02 19:44:52.213917
+#  Source Data: ASRIS 2001, 
+#               ASRIS National Soil Grid,
+#               AU Dept. of Ag. National scale land use data
+#
+#
+# THIS FILE AND THE CONTAINED DATA IS PROVIDED BY THE UNIVERSITY OF IDAHO 
+# 'AS IS' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED 
+# TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A 
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL UNIVERSITY OF IDAHO 
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHElementTreeHER IN 
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+# ARISING IN ANY WAY OUT OF THE USE OF THIS FILE, EVEN IF ADVISED OF THE 
+# POSSIBILITY OF SUCH DAMAGE.
+# 
+#
+#
+# ASRIS 2001 Soil Parameters
+#################################################
+# Bulk Density Topsoil g/cm3               1.012
+# Bulk Density Subsoil g/cm3                1.25
+# Clay Content Topsoil %                    30.0
+# Clay Content Subsoil %                    40.0
+# Silt Content Topsoil                      28.0
+# Silt Content Subsoil                      23.0
+# Sand Content Topsoil                      43.0
+# Sand Content Subsoil                      36.0
+# Texture Topsoil                            4.0
+# Texture Subsoil                            2.0
+# Available Water Topsoil mm                43.0
+# Available Water Subsoil mm               150.0
+# Saturated Hydraulic Topsoil mm/hr        250.0
+# Saturated Hydraulic Subsoil mm/hr        91.25
+# Topsoil Thickness m                      0.218
+# Subsoil Thickness m                      0.975
+# Solum Thickness m                        1.262
+# Organic Carbon Topsoil %                 4.384
+# Organic Carbon Subsoil %                 0.897
+# Total Nitrogen Topsoil %                 0.256
+# Extractable Phosphorus (NSW/VIC)         0.002
+# Total Phosphorus Topsoil                 0.023
+# pH Topsoil                                4.75
+# pH Subsoil                                 4.2
+#
+# hash = ceea8d96d69f8e225092b521951162ab94293ae4bae29380eebb6c43
+#
+#
+# ASRIS National Soil Grid Parameters
+#################################################
+# asc                                 Dermosols 
+# clay30                                      30
+# pawc_1m                                 347383
+# bd30                                         1
+# pH                                           4
+#
+#
+# Land IS Forest (landuse DOM:f3)
+#
+#
+Any comments:
+1 1
+'Dermosols' 	'clay loam' 	1 	0.06 	0.5 	8106800.0 	0.01038212445461566 	2.996 	250.0
+    218.0 	43.0 	30.0 	4.384 	25.0 	15.0
+    436.0 	36.0 	40.0 	0.897 	25.0 	15.0
+1 	25.0 	0.00036
