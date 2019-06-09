39265a9b807115ec85a405c790478519ad3da7ca
Roger Lew <rogerlew@gmail.com>
2019-06-09 12:41:27 -0700

    ash model

--- wepppy/nodb/mods/__init__.py
+++ wepppy/nodb/mods/__init__.py
@@ -2,6 +2,7 @@ from .lt import LakeTahoe
 from .baer import Baer
 from .rred import Rred
 from .debris_flow import DebrisFlow
+from .ash_transport import Ash
 
 import os
 MODS_DIR = os.path.dirname(__file__)
