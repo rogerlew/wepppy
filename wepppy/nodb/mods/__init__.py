6c26b6c0e27eb8c10419531eb3c76f2d2a265250
Roger Lew <rogerlew@gmail.com>
2019-07-04 14:51:58 -0700

    rhem integration

--- wepppy/nodb/mods/__init__.py
+++ wepppy/nodb/mods/__init__.py
@@ -3,6 +3,9 @@ from .baer import Baer
 from .rred import Rred
 from .debris_flow import DebrisFlow
 from .ash_transport import Ash
+from .shrubland import Shrubland, nlcd_shrubland_layers
+from .rangeland_cover import RangelandCover, RangelandCoverMode
+from .rhem import Rhem, RhemPost
 
 import os
 MODS_DIR = os.path.dirname(__file__)
