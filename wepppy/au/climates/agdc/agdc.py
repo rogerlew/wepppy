f90c12264def0e857ca9bd4ff4292be3ca874ffe
Roger Lew <rogerlew@gmail.com>
2019-10-24 12:51:30 -0700

    linveh portland data

---
+++ wepppy/au/climates/agdc/agdc.py
@@ -0,0 +1,15 @@
+# Copyright (c) 2016-2018, University of Idaho
+# All rights reserved.
+#
+# Roger Lew (rogerlew@gmail.com)
+#
+
+from wepppy.climates.cligen import par_mod
+
+
+def agdc_mod(par: int, years: int, lng: float, lat: float, wd: str,
+              nwds_method='', randseed=None, cliver=None, suffix='', logger=None):
+
+    return par_mod(par=par, years=years, lng=lng, lat=lat, wd=wd, monthly_dataset='agdc',
+                   nwds_method=nwds_method, randseed=randseed, cliver=cliver, suffix=suffix,
+                   logger=logger)
