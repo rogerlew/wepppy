26e6635f8e206233bb27657180c9ef49ea10d70b
Roger Lew <rogerlew@gmail.com>
2019-11-12 13:18:01 -0800

    portland bedrock data

---
+++ wepppy/soils/ssurgo/tests/testsoils/2485028-0.04.sol
@@ -0,0 +1,61 @@
+7778
+# 
+#             WEPPcloud v.0.1.0 (c) University of Idaho
+# 
+#   Build Date: 2019-04-25 11:20:22.955624
+#   Source Data: Surgo
+# 
+# Mukey: 2485028
+# Major Component: 16147234 (comppct_r = 45.0)
+# Texture: sand loam
+# 
+#   Chkey   hzname  mask hzdepb_r  ksat_r fraggt10_r frag3to10_r dbthirdbar_r
+# ----------------------------------------------------------------------------
+#  46855068   Oi              4.0   350.0        0.0         0.0          0.2
+#  46855065   A              14.0     3.0        0.0         4.0         1.21
+#  46855066   Bt     R       49.0     1.0        0.0         9.0         1.26
+#  46855067   Bk             92.0     3.0        0.0         0.0         1.33
+#  46855064   Cr            150.0     0.2         -           -           1.4
+# 
+# Restricting Layer:
+#     ksat threshold: 2.00000
+#     type: Paralithic bedrock
+#     ksat: 0.01000
+# 
+# defaults applied to missing chorizon data:
+#     sandtotal_r  ->      66.800
+#     claytotal_r  ->       7.000
+#     om_r         ->       7.000
+#     cec7_r       ->      11.300
+#     sandvf_r     ->      10.000
+#     ksat_r       ->      28.000
+#     dbthirdbar_r ->       1.400
+#     smr          ->      55.500
+#     field_cap    ->       0.242
+#     wilt_pt      ->       0.115
+# 
+# Build Notes:
+#   initial assumed ksat = 0.750
+# THIS FILE AND THE CONTAINED DATA IS PROVIDED BY THE UNIVERSITY OF IDAHO 
+# 'AS IS' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED 
+# TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A 
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL UNIVERSITY OF IDAHO 
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHElementTreeHER IN 
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+# ARISING IN ANY WAY OUT OF THE USE OF THIS FILE, EVEN IF ADVISED OF THE 
+# POSSIBILITY OF SUCH DAMAGE.
+# 
+# 
+#   If you change the original contexts of this file please 
+#   indicate it by putting an 'X' in the box here -> [ ]
+# 
+# 
+Any comments:
+1 1
+'Duckcreek-Cloud Peak-Spanpeak families, complex, 15 to 50 percent slopes'		'SPM'	2	0.0900	0.7500	4649000.00	0.0050	2.5450	0.0000
+	   40.000	    0.20	1260.0000	 10.0	   0.2420	   0.1145	  66.80	   7.00	  75.00	  11.30	  55.50
+	  210.000	    1.21	  10.8000	 10.0	   0.3571	   0.2209	  34.40	  30.00	   5.00	  25.50	  12.64
+1 10000.0 0.04
