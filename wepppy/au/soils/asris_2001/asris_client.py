8b46fb9a975c0e14a64fa35ed318476e5487fccb
Roger Lew <rogerlew@gmail.com>
2020-02-21 09:07:59 -0800

    defaults parameters for when ASRIS (AU Soils) doesn't have data

--- wepppy/au/soils/asris_2001/asris_client.py
+++ wepppy/au/soils/asris_2001/asris_client.py
@@ -5,6 +5,8 @@ import os
 from os.path import join as _join
 from os.path import exists as _exists
 
+from wepppy.all_your_base import isfloat
+
 _thisdir = os.path.dirname(__file__)
 _cache_dir = '/geodata/au/asris_2001/cache'
 
@@ -19,6 +21,34 @@ _url = 'http://www.asris.csiro.au/arcgis/rest/services/ASRIS/ASRIS_2001/MapServe
        'dynamicLayers=&returnZ=false&returnM=false&gdbVersion=&returnUnformattedValues=false&' \
        'returnFieldName=false&datumTransformations=&layerParameterValues=&mapRangeValues=&' \
        'layerRangeValues=&f=pjson'
+
+
+_defaults = {
+    'Bulk Density Topsoil g/cm3': 0.975,
+    'Bulk Density Subsoil g/cm3': 1.2,
+    'Clay Content Topsoil %': 21.0,
+    'Clay Content Subsoil %': 39.0,
+    'Silt Content Topsoil': 27.0,
+    'Silt Content Subsoil': 33.0,
+    'Sand Content Topsoil': 52.0,
+    'Sand Content Subsoil': 29.0,
+    'Texture Topsoil': 4.0,
+    'Texture Subsoil': 2.0,
+    'Available Water Topsoil mm': 41.0,
+    'Available Water Subsoil mm': 80.75,
+    'Saturated Hydraulic Topsoil mm/hr': 97.5,
+    'Saturated Hydraulic Subsoil mm/hr': 100.0,
+    'Topsoil Thickness m': 0.2,
+    'Subsoil Thickness m': 0.475,
+    'Solum Thickness m': 0.65,
+    'Organic Carbon Topsoil %': 7.227,
+    'Organic Carbon Subsoil %': 1.75,
+    'Total Nitrogen Topsoil %': 0.396,
+    'Extractable Phosphorus (NSW/VIC)': 0.001,
+    'Total Phosphorus Topsoil': 0.036,
+    'pH Topsoil': 4.25,
+    'pH Subsoil': 4.3
+}
 
 
 def query_asris(lng, lat):
@@ -47,6 +77,9 @@ def query_asris(lng, lat):
 
     _d = {row['layerName'].replace(' (value/1000)', ''): row for row in d}
     for name in _d:
-        _d[name]['Value'] = float(_d[name]['attributes']['Pixel Value']) / 1000
+        if isfloat(_d[name]['attributes']['Pixel Value']):
+            _d[name]['Value'] = float(_d[name]['attributes']['Pixel Value']) / 1000
+        else:
+            _d[name]['Value'] = _defaults[name]
 
     return _d
