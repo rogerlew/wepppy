07823b7be8c59c541bac361f30500e3d8ad38ec4
Roger Lew <rogerlew@gmail.com>
2020-03-18 10:01:06 -0700

    ashpost

--- wepppy/nodb/mods/__init__.py
+++ wepppy/nodb/mods/__init__.py
@@ -2,7 +2,7 @@ from .lt import LakeTahoe
 from .baer import Baer
 from .rred import Rred
 from .debris_flow import DebrisFlow
-from .ash_transport import Ash
+from .ash_transport import Ash, AshPost
 from .shrubland import Shrubland, nlcd_shrubland_layers
 from .rangeland_cover import RangelandCover, RangelandCoverMode
 from .rhem import Rhem, RhemPost
