081254966df3e63261fc7843fb7d2873f58cfadc
Roger Lew <rogerlew@gmail.com>
2020-08-04 15:46:37 -0700

    LT compilation

---
+++ wepppy/_scripts/lt_merge_arc_exports.py
@@ -0,0 +1,72 @@
+import shutil
+import sys
+import os
+from os.path import join as _join
+from os.path import exists as _exists
+
+from glob import glob
+
+from ogrmerge import process
+
+if __name__ == "__main__":
+    import sys
+
+    outdir = '/home/roger/lt2020_7'
+
+    if _exists(outdir):
+        res = input('Outdir exixsts, Delete outdir?')
+        if not res.lower().startswith('y'):
+            sys.exit()
+
+        shutil.rmtree(outdir)
+
+    os.mkdir(outdir)
+
+    scenarios = [
+                 'SimFire.2020.ki5krcs.chn_cs*_fccsFuels_obs_cli',
+                 'SimFire.2020.ki5krcs.chn_cs*_landisFuels_obs_cli',
+                 'SimFire.2020.ki5krcs.chn_cs*_landisFuels_fut_cli_A2',
+                 'CurCond.2020.cl532.ki5krcs.chn_cs**',
+                 'PrescFireS.2020.ki5krcs.chn_cs*',
+                 'LowSevS.2020.ki5krcs.chn_cs*',
+                 'ModSevS.2020.ki5krcs.chn_cs*',
+                 'HighSevS.2020.ki5krcs.chn_cs*',
+                 'Thinn96.2020.ki5krcs.chn_cs*',
+                 'Thinn93.2020.ki5krcs.chn_cs*',
+                 'Thinn85.2020.ki5krcs.chn_cs*'
+                ]
+
+    for prefix in scenarios:
+        wds = glob(_join('/geodata/weppcloud_runs', '*{}*'.format(prefix)))
+        wds = [wd for wd in wds if os.path.isdir(wd)]
+
+        channels = []
+        subcatchments = []
+
+        for i, wd in enumerate(wds):
+            if wd.endswith('.zip'):
+                continue
+
+            print(wd)
+
+            chn = _join(wd, 'export', 'arcmap', 'channels.shp')
+            assert _exists(chn), chn
+            channels.append(chn)
+
+            sub = _join(wd, 'export', 'arcmap', 'subcatchments.shp')
+            assert _exists(sub), sub
+            subcatchments.append(sub)
+
+        print(channels)
+        print(sub)
+
+        argv = ['-o', '%s/%s_channels.shp' % (outdir, prefix), '-single'] + channels
+        print(argv)
+        process(argv)
+
+        argv = ['-o', '%s/%s_subcatchments.shp' % (outdir, prefix), '-single'] + subcatchments
+        print(argv)
+        process(argv)
+
+
+        print('merged shps are in', outdir)
