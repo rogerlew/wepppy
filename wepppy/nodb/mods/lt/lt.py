fd8490b9d32ee0c547b6f4447061fc6cee81b4f0
Roger Lew <rogerlew@gmail.com>
2020-08-17 16:57:22 -0700

    Update lt.py

--- wepppy/nodb/mods/lt/lt.py
+++ wepppy/nodb/mods/lt/lt.py
@@ -116,7 +116,7 @@ class LakeTahoe(NoDbBase):
             landuse.unlock('-f')
             raise
 
-    def modify_soils(self, default_wepp_type='Granitic'):
+    def modify_soils(self, default_wepp_type='Volcanic'):
         wd = self.wd
         soils_dir = self.soils_dir
         
