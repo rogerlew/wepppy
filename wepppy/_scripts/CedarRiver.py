904c0f67298f0026b23bea6bc3ede27de3f20f8c
Roger Lew <rogerlew@gmail.com>
2020-09-15 13:13:55 -0700

    Update CedarRiver.py

--- wepppy/_scripts/CedarRiver.py
+++ wepppy/_scripts/CedarRiver.py
@@ -171,7 +171,7 @@ if __name__ == '__main__':
             projects[-1]['build_climates'] = scenario['build_climates']
             projects[-1]['lc_lookup_fn'] = scenario['lc_lookup_fn']
             projects[-1]['ksat'] = scenario['ksat']
-            projects[-1]['wd'] = 'seattle_{watershed}_{scenario}' \
+            projects[-1]['wd'] = 'seattle_k_{watershed}_{scenario}' \
                 .format(watershed=watershed['watershed'], scenario=scenario['wd']) \
                 .format(cs=watershed['cs'])
 
