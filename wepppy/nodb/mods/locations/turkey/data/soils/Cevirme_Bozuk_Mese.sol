cabed9f2143ee81010474f68ad7a6ef76b260f50
Roger Lew <rogerlew@gmail.com>
2020-10-01 07:59:53 -0700

    turkey

---
+++ wepppy/nodb/mods/locations/turkey/data/soils/Cevirme_Bozuk_Mese.sol
@@ -0,0 +1,11 @@
+2006.2
+#
+# Created by WpSlp, Thu Sep 03 03:02:21 PM 2020
+# Author: Your Name Here
+#
+Any comments:
+1 1
+'Cevirme_Bozuk_Mese'		'sandy loam'	1 	0.3000 	0.5000 	400000.0000 	0.0005 	2.0000 	50.0000
+	400.000 	55.000 	10.000 	5.000 	15.000 	25.000
+1 25.000000 0.00036
+255  128  0
