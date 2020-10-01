d92340265b4519314c5602a3eef6ecb7846d5f6f
Roger Lew <rogerlew@gmail.com>
2020-10-01 07:59:53 -0700

    turkey

---
+++ wepppy/nodb/mods/locations/turkey/data/soils/Cevirme_Mera.sol
@@ -0,0 +1,11 @@
+2006.2
+#
+# Created by WpSlp, Thu Sep 03 02:57:13 PM 2020
+# Author: Your Name Here
+#
+Any comments:
+1 1
+'cevirme_mera'		'sandy loam'	1 	0.3000 	0.5000 	400000.0000 	0.0005 	2.0000 	50.0000
+	400.000 	65.000 	10.000 	1.000 	15.000 	25.000
+1 25.000000 0.00036
+255  128  0
