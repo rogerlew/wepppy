e2e5b1d1070205b74adbb668abcf7145642f2eb3
Roger Lew <rogerlew@gmail.com>
2020-10-13 11:11:52 -0700

    nodb.mods.location.general

--- wepppy/nodb/base.py
+++ wepppy/nodb/base.py
@@ -211,6 +211,10 @@ class NoDbBase(object):
             seattle = wepppy.nodb.mods.locations.SeattleMod.getInstance(self.wd)
             seattle.on(evt)
 
+        if 'general' in self.mods:
+            general = wepppy.nodb.mods.locations.GeneralMod.getInstance(self.wd)
+            general.on(evt)
+
         if 'baer' in self.mods:
             baer = wepppy.nodb.mods.Baer.getInstance(self.wd)
             baer.on(evt)
--- wepppy/nodb/mods/locations/__init__.py
+++ wepppy/nodb/mods/locations/__init__.py
@@ -2,3 +2,4 @@ from .lt import LakeTahoe
 from .portland import PortlandMod
 from .seattle import SeattleMod
 from .turkey import TurkeyMod
+from .general import GeneralMod
