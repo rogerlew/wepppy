ae23c7fc8273a1aa2419e90ba59fa51154c20d35
Roger Lew <rogerlew@gmail.com>
2020-10-13 11:12:22 -0700

    nodb.mods.locations.general

--- wepppy/nodb/watershed.py
+++ wepppy/nodb/watershed.py
@@ -185,7 +185,7 @@ class Watershed(NoDbBase):
             self.dump_and_unlock()
 
             ron = wepppy.Ron.getInstance(self.wd)
-            if any(['lt' in ron.mods, 'portland' in ron.mods, 'seattle' in ron.mods]):
+            if any(['lt' in ron.mods, 'portland' in ron.mods, 'seattle' in ron.mods, 'general' in ron.mods]):
                 wepp = wepppy.nodb.Wepp.getInstance(self.wd)
                 wepp.trigger(TriggerEvents.PREPPING_PHOSPHORUS)
 
