7f6d20beaf8a40058ffbd5b507befca93cfe8554
Roger Lew <rogerlew@gmail.com>
2020-10-21 13:12:12 -0700

    updated scripts without cell_width parameter

--- wepppy/_scripts/lt_obs_runs.py
+++ wepppy/_scripts/lt_obs_runs.py
@@ -388,7 +388,7 @@ if __name__ == '__main__':
 
             log_print('abstracting watershed')
             wat = Watershed.getInstance(wd)
-            wat.abstract_watershed(cell_width=None)
+            wat.abstract_watershed()
             translator = wat.translator_factory()
             topaz_ids = [top.split('_')[1] for top in translator.iter_sub_ids()]
 
