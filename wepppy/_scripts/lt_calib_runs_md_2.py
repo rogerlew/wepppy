08ae114fc446496955f8669b7c107a783590a2a8
Roger Lew <rogerlew@gmail.com>
2020-10-27 14:38:19 -0700

    Update lt_calib_runs_md_2.py

--- wepppy/_scripts/lt_calib_runs_md_2.py
+++ wepppy/_scripts/lt_calib_runs_md_2.py
@@ -340,8 +340,8 @@ if __name__ == '__main__':
                     continue
 
             if skip_completed:
-                if _exists(_join(wd, 'wepp', 'output', 'loss_pw0.txt')):
-                    log_print('has loss_pw0.txt... skipping.')
+                if _exists(_join(wd, 'export', 'arcmap', 'channels.shp')):
+                    log_print('has channels.shp... skipping.')
                     continue
 
             log_print('cleaning dir')
