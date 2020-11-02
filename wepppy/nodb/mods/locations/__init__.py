d701a80badd3a11ad007073596171ffad29524a2
Roger Lew <rogerlew@gmail.com>
2020-11-02 13:09:09 -0800

    Add wepppy-locations-seattle submodule

--- wepppy/nodb/mods/locations/__init__.py
+++ wepppy/nodb/mods/locations/__init__.py
@@ -1,5 +1,5 @@
 from .lt import LakeTahoe
 from .wepppy_locations_portland import PortlandMod
-from .seattle import SeattleMod
+from .wepppy_locations_seattle import SeattleMod
 from .turkey import TurkeyMod
 from .general import GeneralMod
