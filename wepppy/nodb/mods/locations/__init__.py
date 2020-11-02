70e646eee14ee928950336fc77cb1edbb5990071
Roger Lew <rogerlew@gmail.com>
2020-11-02 13:28:30 -0800

    gracefully ignore portlandmod importerror

--- wepppy/nodb/mods/locations/__init__.py
+++ wepppy/nodb/mods/locations/__init__.py
@@ -1,5 +1,13 @@
 from .lt import LakeTahoe
-from .wepppy_locations_portland import PortlandMod
-from .wepppy_locations_seattle import SeattleMod
+try:
+    from .wepppy_locations_portland import PortlandMod
+except ImportError:
+    pass
+
+try:
+    from .wepppy_locations_seattle import SeattleMod
+except ImportError:
+    pass
+
 from .turkey import TurkeyMod
 from .general import GeneralMod
