39e292f494d806c0510bed31a587813f2afb6eb5
Roger Lew <rogerlew@gmail.com>
2020-11-07 01:13:48 -0800

    rename tests to _tests

---
+++ wepppy/_tests/feverish-lamp/soils/1487714.sol
@@ -0,0 +1,62 @@
+7778
+# 
+#             WEPPcloud v.0.1.0 (c) University of Idaho
+# 
+#   Build Date: 2020-03-13 12:16:24.246375
+#   Source Data: Surgo
+# 
+# Mukey: 1487714
+# Major Component: 15771282 (comppct_r = 80.0)
+# Texture: silt loam
+# 
+#   Chkey   hzname  mask hzdepb_r  ksat_r fraggt10_r frag3to10_r dbthirdbar_r    clay    sand     vfs      om
+# ------------------------------------------------------------------------------------------------------------
+#  45673416   Ap             20.0    13.2        0.0         0.0          1.1    15.0    10.0     6.5    3.25
+#  45673419   Bw             48.0    13.2        0.0         0.0         1.47    15.0    10.0     6.5     1.1
+#  45673418   E              74.0     4.8        0.0         0.0         1.47    12.0     9.0     5.5    0.63
+#  45673417   Btxb1  R       97.0    0.07        0.0         0.0          1.6    26.0     6.7     4.7    0.46
+#  45673420   Btxb2         150.0    0.07        0.0         0.0         1.64    26.0     6.7     4.7    0.33
+# 
+# Restricting Layer:
+#     ksat threshold: 2.00000
+#     type: Fragipan
+#     ksat: 0.07000
+# 
+# defaults applied to missing chorizon data:
+#     sandtotal_r  ->      66.800
+#     claytotal_r  ->       7.000
+#     om_r         ->       7.000
+#     cec7_r       ->      11.300
+#     sandvf_r     ->      10.000
+#     ksat_r       ->      28.000
+#     dbthirdbar_r ->       1.400
+#     smr          ->      55.500
+#     field_cap    ->       0.242
+#     wilt_pt      ->       0.115
+# 
+# Build Notes:
+#   initial assumed ksat = 0.750
+# THIS FILE AND THE CONTAINED DATA IS PROVIDED BY THE UNIVERSITY OF IDAHO 
+# 'AS IS' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED 
+# TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A 
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL UNIVERSITY OF IDAHO 
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHElementTreeHER IN 
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+# ARISING IN ANY WAY OUT OF THE USE OF THIS FILE, EVEN IF ADVISED OF THE 
+# POSSIBILITY OF SUCH DAMAGE.
+# 
+# 
+#   If you change the original contexts of this file please 
+#   indicate it by putting an 'X' in the box here -> [ ]
+# 
+# 
+Any comments:
+1 1
+'Santa ashy silt loam, 8 to 15 percent slopes'		'ASHY-SIL'	3	0.1600	0.7500	5227050.00	0.0136	3.5000	0.0000
+	  200.000	    1.10	  47.5200	 10.0	   0.2350	   0.0770	  10.00	  15.00	   3.25	  13.40	   0.00
+	  480.000	    1.47	  47.5200	 10.0	   0.2610	   0.1030	  10.00	  15.00	   1.10	  12.90	   0.00
+	  740.000	    1.47	  17.2800	  1.0	   0.2380	   0.0790	   9.00	  12.00	   0.63	  10.30	   0.00
+1 10000.0 0.25200
