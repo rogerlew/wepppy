b61e4eaac16baf298fe5e290645629c7e16637e1
Roger Lew <rogerlew@gmail.com>
2020-11-07 01:13:48 -0800

    rename tests to _tests

---
+++ wepppy/_tests/feverish-lamp/soils/2487291.sol
@@ -0,0 +1,59 @@
+7778
+# 
+#             WEPPcloud v.0.1.0 (c) University of Idaho
+# 
+#   Build Date: 2020-03-13 12:16:24.248928
+#   Source Data: Surgo
+# 
+# Mukey: 2487291
+# Major Component: 15990722 (comppct_r = 85.0)
+# Texture: silt loam
+# 
+#   Chkey   hzname  mask hzdepb_r  ksat_r fraggt10_r frag3to10_r dbthirdbar_r    clay    sand     vfs      om
+# ------------------------------------------------------------------------------------------------------------
+#  46407687   AB             46.0     9.0        0.0         0.0         1.45    20.0    11.4     6.6     3.5
+#  46407688   Bt/Ec          97.0     9.0        0.0         0.0          1.5    15.0    14.0     8.5     1.5
+#  46407689   Btcx   R      152.0    0.21        0.0         0.0          1.7    31.0     6.9     2.8    0.75
+# 
+# Restricting Layer:
+#     ksat threshold: 2.00000
+#     type: N/A
+#     ksat: 0.21000
+# 
+# defaults applied to missing chorizon data:
+#     sandtotal_r  ->      66.800
+#     claytotal_r  ->       7.000
+#     om_r         ->       7.000
+#     cec7_r       ->      11.300
+#     sandvf_r     ->      10.000
+#     ksat_r       ->      28.000
+#     dbthirdbar_r ->       1.400
+#     smr          ->      55.500
+#     field_cap    ->       0.242
+#     wilt_pt      ->       0.115
+# 
+# Build Notes:
+#   initial assumed ksat = 0.750
+# THIS FILE AND THE CONTAINED DATA IS PROVIDED BY THE UNIVERSITY OF IDAHO 
+# 'AS IS' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED 
+# TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A 
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL UNIVERSITY OF IDAHO 
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHElementTreeHER IN 
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+# ARISING IN ANY WAY OUT OF THE USE OF THIS FILE, EVEN IF ADVISED OF THE 
+# POSSIBILITY OF SUCH DAMAGE.
+# 
+# 
+#   If you change the original contexts of this file please 
+#   indicate it by putting an 'X' in the box here -> [ ]
+# 
+# 
+Any comments:
+1 1
+'Southwick silt loam, 25 to 35 percent slopes'		'SIL'	2	0.2300	0.7500	4951400.00	0.0094	3.5000	0.0000
+	  460.000	    1.45	  32.4000	 10.0	   0.3060	   0.1630	  11.40	  20.00	   3.50	  17.40	   0.00
+	  970.000	    1.50	  32.4000	  1.0	   0.2680	   0.1110	  14.00	  15.00	   1.50	  13.00	   0.00
+1 10000.0 0.75600
