cac228577f44f5681d93099e81695ed0aa8c551d
Roger Lew <rogerlew@gmail.com>
2020-12-01 15:45:59 -0800

    export management pars for disturbed

--- wepppy/wepp/management/tests/export_covers.py
+++ wepppy/wepp/management/tests/export_covers.py
@@ -1,13 +1,26 @@
 import csv
 from wepppy.wepp.management import load_map, get_management, IniLoopCropland
 
-db = None  # 'lu10v5ua' #'esdac' # 'rred' # 'disturbed
+db = 'disturbed'  # None  # 'lu10v5ua' #'esdac' # 'rred' # 'disturbed
 d = load_map(db)
 
-fp = open('tests/weppcloud_managements.csv', 'w')
+fp = open('%s_weppcloud_managements.csv' % str(db), 'w')
 wtr = csv.writer(fp)
 
-wtr.writerow(['key', 'desc', 'man', 'cancov', 'inrcov', 'rilcov'])
+hdr = ['key', 'desc', 'man',
+       'ini.data.bdtill', 'ini.data.cancov', 'ini.data.daydis', 'ini.data.dsharv', 'ini.data.frdp', 
+       'ini.data.inrcov', 'ini.data.iresd', 'ini.data.imngmt', 'ini.data.rfcum', 'ini.data.rhinit',
+       'ini.data.rilcov', 'ini.data.rrinit', 'ini.data.rspace', 'ini.data.rtyp', 'ini.data.snodpy',
+       'ini.data.thdp', 'ini.data.tillay1', 'ini.data.tillay2', 'ini.data.width', 'ini.data.sumrtm',
+       'ini.data.sumsrm',        
+       'plant.data.bb', 'plant.data.bbb', 'plant.data.beinp', 'plant.data.btemp', 'plant.data.cf', 
+       'plant.data.crit', 'plant.data.critvm', 'plant.data.cuthgt', 'plant.data.decfct', 'plant.data.diam', 
+       'plant.data.dlai', 'plant.data.dropfc', 'plant.data.extnct', 'plant.data.fact', 'plant.data.flivmx', 
+       'plant.data.gddmax', 'plant.data.hi', 'plant.data.hmax',
+       'plant.data.mfocod',
+       'plant.data.oratea', 'plant.data.orater', 'plant.data.otemp', 'plant.data.pltol']
+
+wtr.writerow([v.replace('.data', '') for v in  hdr])
 
 for k in d:
     m = get_management(k, _map=db)
@@ -19,8 +32,13 @@ for k in d:
     cancov, inrcov, rilcov = m.inis[0].data.cancov, m.inis[0].data.inrcov, m.inis[0].data.rilcov
     man_fn = d[k]['ManagementFile']
 
-    print('{},{},{},{},{}'.format(k, m.desc, man_fn, cancov, inrcov, rilcov))
+    template = ','.join(['{%s}' % v for v in hdr])
 
-    wtr.writerow([k, m.desc, cancov, inrcov, rilcov])
+    row = template.format(key=k, desc=m.desc, man=man_fn, 
+                          ini=m.inis[0],
+                          plant=m.plants[0]).split(',')
+    print(row)
+
+    wtr.writerow(row)
 
 fp.close()
