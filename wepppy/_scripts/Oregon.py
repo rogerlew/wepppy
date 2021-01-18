25a0fb34d1c8a6f9c17e1e66f7af072c58709989
marianadobre <37674030+marianadobre@users.noreply.github.com>
2021-01-18 11:55:23 -0800

    Update Oregon.py

--- wepppy/_scripts/Oregon.py
+++ wepppy/_scripts/Oregon.py
@@ -387,7 +387,7 @@ if __name__ == '__main__':
 
                 climate.climate_mode = ClimateMode.GridMetPRISM
                 climate.climate_spatialmode = ClimateSpatialMode.Multiple
-                climate.set_observed_pars(start_year=1980, end_year=2020)
+                climate.set_observed_pars(start_year=1980, end_year=2021)
 
                 climate.build(verbose=1)
 
