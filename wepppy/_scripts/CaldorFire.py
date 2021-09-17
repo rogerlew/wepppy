60b61ce704353e0759f97fb003a918f8abd01c81
marianadobre <37674030+marianadobre@users.noreply.github.com>
2021-09-17 15:32:00 -0700

    Update CaldorFire.py

--- wepppy/_scripts/CaldorFire.py
+++ wepppy/_scripts/CaldorFire.py
@@ -283,7 +283,7 @@ if __name__ == '__main__':
 
                 climate.climate_mode = ClimateMode.GridMetPRISM
                 climate.climate_spatialmode = ClimateSpatialMode.Multiple
-                climate.set_observed_pars(start_year=1990, end_year=1990)
+                climate.set_observed_pars(start_year=1990, end_year=1991)
 
                 climate.build(verbose=1)
 
