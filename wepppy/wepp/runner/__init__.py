fab76de181c77c4f87bfe73d58a6c299f2c2852c
Roger Lew <rogerlew@gmail.com>
2021-11-04 11:08:56 -0700

    bug fix for running single storm with flowpaths

--- wepppy/wepp/runner/__init__.py
+++ wepppy/wepp/runner/__init__.py
@@ -6,6 +6,7 @@ from .wepp_runner import (
     make_ss_hillslope_run,
     run_hillslope,
     make_flowpath_run,
+    make_ss_flowpath_run,
     run_flowpath,
     make_watershed_run,
     make_ss_watershed_run,
