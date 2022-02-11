b59bd334ef28ab8b7cb07c5261258d30bb3b9116
Roger Lew <rogerlew@gmail.com>
2022-02-11 11:54:38 -0800

    app.py WEPPcloudR routes

--- wepppy/weppcloud/app.py
+++ wepppy/weppcloud/app.py
@@ -4384,6 +4384,7 @@ def submit_task_run_rhem(runid, config):
 
     return success_factory()
 
+import pathlib
 import io
 VIZ_RSCRIPT_DIR = '/workdir/viz-weppcloud/scripts/R/'
 VIZ_RMARKDOWN_DIR = '/workdir/viz-weppcloud/scripts/Rmd'
@@ -4445,6 +4446,161 @@ def viz_r(runid, config, r_format, routine):
 
     except:
         return exception_factory('Error running script')
+
+
+WEPPCLOUDR_DIR = '/workdir/WEPPcloudR/scripts'
+
+
+def _weppcloudr_script_locator(routine, user=None):
+    global WEPPCLOUDR_DIR 
+    if user is None:
+        return _join(WEPPCLOUDR_DIR, routine)
+    else:
+        rscript =  _join(WEPPCLOUDR_DIR, 'users', user, routine)
+        assert pathlib.Path(WEPPCLOUDR_DIR) in  pathlib.Path(rscript).parents
+        return rscript
+
+
+@app.route('/runs/<string:runid>/<config>/WEPPcloudR/<routine>')
+@app.route('/runs/<string:runid>/<config>/WEPPcloudR/<routine>/')
+def weppcloudr(runid, config, routine):
+    assert config is not None
+
+    wd = get_wd(runid)
+    owners = get_run_owners(runid)
+    try:
+        ron = Ron.getInstance(wd)
+    except FileNotFoundError:
+        abort(404)
+
+    should_abort = True
+    if current_user in owners:
+        should_abort = False
+
+    if not owners:
+        should_abort = False
+
+    if current_user.has_role('Admin'):
+        should_abort = False
+
+    if ron.public:
+        should_abort = False
+
+    if should_abort:
+        abort(404)
+
+    user = request.args.get('user', None)
+
+    viz_export_dir = _join(wd, 'export/WEPPcloudR')
+    if not _exists(viz_export_dir):
+        os.mkdir(viz_export_dir)
+        
+    try:
+        assert routine.endswith('.R') or routine.endswith('.Rmd'), routine
+
+        # routine_stem = '.'.join(routine.split('.')[:-1]) 
+        r_format = routine.split('.')[-1] 
+        rpt_fn = _join(viz_export_dir, f'{routine}.htm')
+
+        rscript = _weppcloudr_script_locator(routine, user=user)
+        assert _exists(rscript)
+
+        if r_format.lower() == 'r':
+            cmd = ['Rscript', rscript, runid]
+        elif r_format.lower() == "rmd":
+            cmd = ['R', '-e', f'library("rmarkdown"); rmarkdown::render("{rscript}", params=list(proj_runid="{runid}"), output_file="{rpt_fn}", output_dir="{viz_export_dir}")']
+
+        p = Popen(cmd, stdout=PIPE, stderr=PIPE)
+        output, errors = p.communicate()
+        with open(_join(viz_export_dir, f'{routine}.stdout'), 'w') as fp:
+            fp.write(output.decode('utf-8'))
+        with open(_join(viz_export_dir, f'{routine}.stderr'), 'w') as fp:
+            fp.write(errors.decode('utf-8'))
+
+        assert _exists(rpt_fn)
+        with io.open(rpt_fn, encoding='utf8') as fp:
+            return fp.read()
+
+    except:
+        return exception_factory('Error running script')
+
+
+
+@app.route('/WEPPcloudR/proxy/<routine>', methods=['GET', 'POST'])
+@app.route('/WEPPcloudR/proxy/<routine>/', methods=['GET', 'POST'])
+def weppcloudr_proxy(routine):
+    if current_user.is_authenticated:
+        if not current_user.roles:
+            user_datastore.add_role_to_user(current_user.email, 'User')
+
+    user = request.args.get('user', None)
+    runids = request.args.get('runids', '')
+    runids = runids.replace(',', ' ').split()
+
+    try:
+
+        ws = []
+        for i, runid in enumerate(runids):
+            if runid.endswith('/'):
+                runid = runid[:-1]
+
+            wd = get_wd(runid)
+            try:
+                ron = Ron.getInstance(wd)
+                wepp = Wepp.getInstance(wd)
+                watershed = Watershed.getInstance(wd)
+            except:
+                raise Exception('Error acquiring nodb instances from ' + wd)
+
+            name = ron.name
+            scenario = ron.scenario
+            w3w = ron.w3w
+            sub_n = watershed.sub_n
+            is_topaz = int(watershed.delineation_backend_is_topaz)
+            location_hash = f'{w3w}_{is_topaz}_{sub_n}'
+
+            ws.append(dict(runid=runid, cfg=ron.config_stem, name=name, scenario=scenario, location_hash=location_hash))
+
+        
+        js = json.dumps(ws)
+
+    except:
+        return exception_factory('Error running script')
+
+    wd = get_wd(runids[0]) 
+    viz_export_dir = _join(wd, 'export/WEPPcloudR')
+    if not _exists(viz_export_dir):
+        os.mkdir(viz_export_dir)
+        
+    try:
+        assert routine.endswith('.R') or routine.endswith('.Rmd'), routine
+
+        # routine_stem = '.'.join(routine.split('.')[:-1]) 
+        r_format = routine.split('.')[-1] 
+        rpt_fn = _join(viz_export_dir, f'{routine}.htm')
+
+        rscript = _weppcloudr_script_locator(routine, user=user)
+        assert _exists(rscript)
+
+        if r_format.lower() == 'r':
+            cmd = ['Rscript', rscript, runid]
+        elif r_format.lower() == "rmd":
+            cmd = ['R', '-e', f'''library("rmarkdown"); rmarkdown::render("{rscript}", params=list(ws='{js}'), output_file="{rpt_fn}", output_dir="{viz_export_dir}")''']
+
+        p = Popen(cmd, stdout=PIPE, stderr=PIPE)
+        output, errors = p.communicate()
+        with open(_join(viz_export_dir, f'{routine}.stdout'), 'w') as fp:
+            fp.write(output.decode('utf-8'))
+        with open(_join(viz_export_dir, f'{routine}.stderr'), 'w') as fp:
+            fp.write(errors.decode('utf-8'))
+
+        assert _exists(rpt_fn)
+        with io.open(rpt_fn, encoding='utf8') as fp:
+            return fp.read()
+
+    except:
+        return exception_factory('Error processing request')
+
 
 #  R -e 'library("rmarkdown"); rmarkdown::render("03_Rmarkdown_to_generate_reports.Rmd", params=list(proj_runid="lt_202012_26_Bliss_Creek_CurCond"), output_file="rmd_rpt.htm")'
 
