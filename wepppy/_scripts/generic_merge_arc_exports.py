930ea7631e007673babed7e315f8af83a4b7a3bb
marianadobre <37674030+marianadobre@users.noreply.github.com>
2022-03-17 15:41:40 -0700

    Update generic_merge_arc_exports.py

--- wepppy/_scripts/generic_merge_arc_exports.py
+++ wepppy/_scripts/generic_merge_arc_exports.py
@@ -18,8 +18,8 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--prefix', type=str, default='prefix', help='prefix for output files')
-    parser.add_argument('--outdir', type=str, default='/home/mariana/BLKWD_UTR_300m_shp', help='outdir for files')
-    parser.add_argument('--input_csv', type=str, default='Blackwood_UTR_300m.csv', help='input file with RunID, WatershedName, Scenario colmans')
+    parser.add_argument('--outdir', type=str, default='/home/mariana/BullRun_march_shp', help='outdir for files')
+    parser.add_argument('--input_csv', type=str, default='BullRun_Runs_ID.csv', help='input file with RunID, WatershedName, Scenario colmans')
 
     args = parser.parse_args()
 
