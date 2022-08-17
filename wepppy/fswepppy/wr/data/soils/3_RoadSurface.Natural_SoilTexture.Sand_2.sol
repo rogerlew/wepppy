61ea1242189023e254ce730476080b5466665c9c
Roger Lew <rogerlew@gmail.com>
2022-08-17 12:58:25 -0700

    observed spotpy measures

---
+++ wepppy/fswepppy/wr/data/soils/3_RoadSurface.Natural_SoilTexture.Sand_2.sol
@@ -0,0 +1,41 @@
+7778
+# 
+# File:    3sand2.sol
+# Created:  ; 08 Oct 2003
+# Author:   ; Darrell Anderson
+# Contact: Bill Elliot, Soil and Water Engineering
+# U.S. Forest Service Rocky Mountain Research Station
+# http://forest.moscowfsl.wsu.edu/fswepp
+# 
+# Insloped bare ditch or outsloped
+# 
+# wepppy.wepp.soils.utils.WeppSoilUtil 7778 migration
+#   Build Date: 2022-08-16 15:09:05.403334
+#   Source File: :og/3sand2.sol
+# 
+# ofe=0,horizon0 bd default value of 1.4
+# ofe=0,horizon0 fc estimated using Rosetta(clay=5, sand=60, bd=None, silt=35)
+# ofe=0,horizon0 wp estimated using Rosetta(clay=5, sand=60, bd=None, silt=35)
+# ofe=0,horizon0 ksat estimated using Rosetta(clay=5, sand=60, bd=None, silt=35)
+# ofe=0,horizon0 anisotropy estimated using Rosetta(clay=5, sand=60, bd=None, silt=35)
+# ofe=1,horizon0 bd default value of 1.4
+# ofe=1,horizon0 fc estimated using Rosetta(clay=5, sand=60, bd=None, silt=35)
+# ofe=1,horizon0 wp estimated using Rosetta(clay=5, sand=60, bd=None, silt=35)
+# ofe=1,horizon0 ksat estimated using Rosetta(clay=5, sand=60, bd=None, silt=35)
+# ofe=1,horizon0 anisotropy estimated using Rosetta(clay=5, sand=60, bd=None, silt=35)
+# ofe=2,horizon0 bd default value of 1.4
+# ofe=2,horizon0 fc estimated using Rosetta(clay=5, sand=60, bd=None, silt=35)
+# ofe=2,horizon0 wp estimated using Rosetta(clay=5, sand=60, bd=None, silt=35)
+# ofe=2,horizon0 ksat estimated using Rosetta(clay=5, sand=60, bd=None, silt=35)
+# ofe=2,horizon0 anisotropy estimated using Rosetta(clay=5, sand=60, bd=None, silt=35)
+Plume for road (12/02/98)
+3 0
+'Road'	 'sandy loam'	 1	 0.6	 0.5	 2000000.0	 0.0004	 2.0
+	200	 1.4	 54.463	 1.0	 0.1938	 0.0809	 60	 5	 0.01	 4	 urr
+1 10000.0 54.463
+'Fill'	 'sandy loam'	 1	 0.12	 0.45	 2000000.0	 0.0004	 2.0
+	300	 1.4	 54.463	 1.0	 0.1938	 0.0809	 60	 5	 4	 4	 ufr
+1 10000.0 54.463
+'Forest'	 'sandy loam'	 1	 0.02	 0.4	 130000.0	 0.0004	 2.0
+	300	 1.4	 54.463	 1.0	 0.1938	 0.0809	 60	 5	 8	 5	 ubr
+1 10000.0 54.463
---
+++ wepppy/fswepppy/wr/data/soils/3_RoadSurface.Natural_SoilTexture.Silt_10.sol
@@ -0,0 +1,41 @@
+7778
+# 
+# File:    3silt10.sol
+# Created: 08 June 1999; 08 Oct 2003
+# Author:  S.R. Graves; Darrell Anderson
+# Contact: Bill Elliot, Soil and Water Engineering
+# U.S. Forest Service Rocky Mountain Research Station
+# http://forest.moscowfsl.wsu.edu/fswepp
+# 
+# Insloped rocked ditch
+# 
+# wepppy.wepp.soils.utils.WeppSoilUtil 7778 migration
+#   Build Date: 2022-08-16 15:09:08.167913
+#   Source File: :og/3silt10.sol
+# 
+# ofe=0,horizon0 bd default value of 1.4
+# ofe=0,horizon0 fc estimated using Rosetta(clay=15, sand=30, bd=None, silt=55)
+# ofe=0,horizon0 wp estimated using Rosetta(clay=15, sand=30, bd=None, silt=55)
+# ofe=0,horizon0 ksat estimated using Rosetta(clay=15, sand=30, bd=None, silt=55)
+# ofe=0,horizon0 anisotropy estimated using Rosetta(clay=15, sand=30, bd=None, silt=55)
+# ofe=1,horizon0 bd default value of 1.4
+# ofe=1,horizon0 fc estimated using Rosetta(clay=15, sand=30, bd=None, silt=55)
+# ofe=1,horizon0 wp estimated using Rosetta(clay=15, sand=30, bd=None, silt=55)
+# ofe=1,horizon0 ksat estimated using Rosetta(clay=15, sand=30, bd=None, silt=55)
+# ofe=1,horizon0 anisotropy estimated using Rosetta(clay=15, sand=30, bd=None, silt=55)
+# ofe=2,horizon0 bd default value of 1.4
+# ofe=2,horizon0 fc estimated using Rosetta(clay=15, sand=30, bd=None, silt=55)
+# ofe=2,horizon0 wp estimated using Rosetta(clay=15, sand=30, bd=None, silt=55)
+# ofe=2,horizon0 ksat estimated using Rosetta(clay=15, sand=30, bd=None, silt=55)
+# ofe=2,horizon0 anisotropy estimated using Rosetta(clay=15, sand=30, bd=None, silt=55)
+Plume for road (12/02/98)
+3 0
+'Road'	 'silt loam'	 1	 0.6	 0.5	 2000000.0	 0.0003	 10
+	200	 1.4	 21.8754	 1.0	 0.2241	 0.1177	 30	 15	 0.01	 12	 urr
+1 10000.0 21.8754
+'Fill'	 'silt loam'	 1	 0.12	 0.45	 2000000.0	 0.0003	 2
+	300	 1.4	 21.8754	 1.0	 0.2241	 0.1177	 30	 15	 4	 13	 ufr
+1 10000.0 21.8754
+'Forest'	 'silt loam'	 1	 0.02	 0.4	 120000.0	 0.0003	 2
+	300	 1.4	 21.8754	 1.0	 0.2241	 0.1177	 30	 15	 8	 14	 ubr
+1 10000.0 21.8754
