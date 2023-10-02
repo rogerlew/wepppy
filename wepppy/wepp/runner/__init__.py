611e03bd98fa7074e4d63afd07ece3993ca4f5db
Roger Lew <rogerlew@gmail.com>
2023-10-02 07:57:42 -0700

    single storm batch, soil depth clip

--- wepppy/wepp/runner/__init__.py
+++ wepppy/wepp/runner/__init__.py
@@ -4,13 +4,17 @@ from os.path import join as _join
 from .wepp_runner import (
     make_hillslope_run,
     make_ss_hillslope_run,
+    make_ss_batch_hillslope_run,
     run_hillslope,
+    run_ss_batch_hillslope,
     make_flowpath_run,
     make_ss_flowpath_run,
     run_flowpath,
     make_watershed_run,
     make_ss_watershed_run,
+    make_ss_batch_watershed_run,
     run_watershed,
+    run_ss_batch_watershed,
     linux_wepp_bin_opts)
 
 _thisdir = os.path.dirname(__file__)
