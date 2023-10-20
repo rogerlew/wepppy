4be2fbaa47ef7aba7a80a00a0fba57aac0f1db3d
Roger Lew <rogerlew@gmail.com>
2023-10-20 12:18:00 -0700

    extract compute_erodibilities and compute_conductivity from WeppSoilUtil

--- wepppy/wepp/soils/horizon_mixin.py
+++ wepppy/wepp/soils/horizon_mixin.py
@@ -35,6 +35,76 @@ def estimate_bulk_density(sand_percent, silt_percent, clay_percent):
     return estimated_density
 
 
+def compute_conductivity(clay: float, sand: float, cec: float):
+
+    conductivity = 0
+
+    # conductivity
+    if sand == 0.0 or clay == 0.0 or cec == 0.0:
+        return None
+
+    if clay <= 40.0:
+        if cec > 1.0:
+            # apply equation 1 from usersum.pdf
+            conductivity = -0.265 + 0.0086 * pow(sand, 1.8) + \
+                           11.46 * pow(cec, -0.75)
+        else:
+            # this isn't documented in the usersum.pdf
+            # this is what is being applied in the watershed
+            # interface
+            conductivity = 11.195 + 0.0086 * pow(sand, 1.8)
+    else:
+        # apply equation 2 from usersum.pdf
+        conductivity = 0.0066 * exp(244.0 / clay)
+
+    return conductivity
+
+
+def compute_erodibilities(clay: float, sand: float, vfs: float, om: float):
+    # interrill, rill, shear
+    if sand == 0.0 or vfs == 0.0 or om == 0.0 or clay == 0.0:
+        return dict(
+            interrill=0.0,
+            rill=0.0,
+            shear=0.0
+        )
+
+    if sand >= 30.0:
+        if vfs > 40.0:
+            vfs = 40.0
+        if om < 0.35:
+            om = 1.36
+        if clay > 42.0:
+            clay = 42.0
+
+        # apply equation 6 from usersum.pdf
+        interrill = 2728000.0 + 192100.0 * vfs
+
+        # apply equation 7 from usersum.pdf
+        rill = 0.00197 + 0.00030 * vfs + 0.03863 * exp(-1.84 * om)
+
+        # apply equation 8 from usersum.pdf
+        shear = 2.67 + 0.065 * clay - 0.058 * vfs
+    else:
+        if clay < 10.0:
+            clay = 10.0
+
+        # apply equation 9 from usersum.pdf
+        interrill = 6054000.0 - 55130.0 * clay
+
+        # apply equation 10 from usersum.pdf
+        rill = 0.0069 + 0.134 * exp(-0.20 * clay)
+
+        # apply equation 11 from usersum.pdf
+        shear = 3.5
+
+    return dict(
+        interrill=interrill,
+        rill=rill,
+        shear=shear
+    )
+
+
 class HorizonMixin(object):
     def _rosettaPredict(self):
         from rosetta import Rosetta2, Rosetta3
@@ -47,7 +117,7 @@ class HorizonMixin(object):
         assert isfloat(clay), clay
         assert isfloat(sand), sand
         assert isfloat(vfs), vfs
-    
+
         if isfloat(bd):
             r3 = Rosetta3()
             res_dict = r3.predict_kwargs(sand=sand, silt=vfs, clay=clay, bd=bd)
@@ -64,95 +134,31 @@ class HorizonMixin(object):
         self.rosetta_d = res_dict
 
     def _computeConductivity(self):
-        clay = self.clay
-        sand = self.sand
-        cec = self.cec
+        self.conductivity = compute_conductivity(clay=self.clay, sand=self.sand, cec=self.cec)
 
-        conductivity = 0 
-        
-        # conductivity
-        if sand == 0.0 or clay == 0.0 or cec == 0.0:
-            self.conductivity = None
-            return
-            
-        if clay <= 40.0:
-            if cec > 1.0:
-                # apply equation 1 from usersum.pdf
-                conductivity = -0.265 + 0.0086 * pow(sand, 1.8) + \
-                               11.46 * pow(cec, -0.75) 
-            else:
-                # this isn't documented in the usersum.pdf
-                # this is what is being applied in the watershed
-                # interface
-                conductivity = 11.195 + 0.0086 * pow(sand, 1.8)  
-        else:
-            # apply equation 2 from usersum.pdf
-            conductivity = 0.0066 * exp(244.0 / clay) 
-            
-        self.conductivity = conductivity
-
-  
     def _computeErodibility(self):
         """
         Computes erodibility estimates according to:
-        
+
         https://www.ars.usda.gov/ARSUserFiles/50201000/WEPP/usersum.pdf
         """
-        clay = self.clay
-        sand = self.sand
-        vfs = self.vfs
-        om = self.om
-        
-        # interrill, rill, shear
-        if sand == 0.0 or vfs == 0.0 or om == 0.0 or clay == 0.0:
-            self.interrill = 0.0
-            self.rill = 1.0
-            self.shear = 0.0
-            return 
-        
-        if sand >= 30.0:
-            if vfs > 40.0:
-                vfs = 40.0
-            if om < 0.35:
-                om = 1.36
-            if clay > 42.0:
-                clay = 42.0
-                
-            # apply equation 6 from usersum.pdf
-            interrill = 2728000.0 + 192100.0 * vfs
-            
-            # apply equation 7 from usersum.pdf
-            rill = 0.00197 + 0.00030 * vfs + 0.03863 * exp(-1.84 * om) 
-            
-            # apply equation 8 from usersum.pdf
-            shear = 2.67 + 0.065 * clay - 0.058 * vfs
-        else:
-            if clay < 10.0:
-                clay = 10.0
-                
-            # apply equation 9 from usersum.pdf
-            interrill = 6054000.0 - 55130.0 * clay
-            
-            # apply equation 10 from usersum.pdf
-            rill = 0.0069 + 0.134 * exp(-0.20 * clay)
-            
-            # apply equation 11 from usersum.pdf
-            shear = 3.5
-      
-        self.interrill = interrill
-        self.rill = rill
-        self.shear = shear
+
+        res = compute_erodibilities(clay=self.clay, sand=self.sand, vfs=self.vfs, om=self.om)
+
+        self.interrill = res['interrill']
+        self.rill = res['rill']
+        self.shear = res['shear']
 
     def _computeAnisotropy(self):
         hzdepb_r = self.depth
-        
+
         anisotropy = None
         if isfloat(hzdepb_r):
             if hzdepb_r > 50:
                 anisotropy = 1.0
             else:
                 anisotropy = 10.0
-                
+
         self.anisotropy = anisotropy
 
     @property
@@ -164,4 +170,4 @@ class HorizonMixin(object):
         """
         from wepppy.wepp.soils.utils import simple_texture
         return simple_texture(self.clay, self.sand)
-  
+
