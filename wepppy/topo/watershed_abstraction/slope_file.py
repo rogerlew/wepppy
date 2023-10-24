2e6ea2a7d0027d5d61ca44918a554d784d46be62
Roger Lew <rogerlew@gmail.com>
2023-10-24 16:42:00 -0700

    mofe interpolate break points

--- wepppy/topo/watershed_abstraction/slope_file.py
+++ wepppy/topo/watershed_abstraction/slope_file.py
@@ -3,43 +3,61 @@ import numpy as np
 from scipy.interpolate import KroghInterpolator
 
 
-def polycurve(x, dy):
-    #
-    # Calculate the y positions from the derivatives
-    #
+def clip_slope_file_length(src_fn, dst_fn, clip_length):
+    with open(src_fn) as fp:
+        lines = fp.readlines()
+        lines = [L for L in lines if not L.startswith('#')]
 
-    # for each segment calculate the average gradient from the derivatives at each point
-    dy = np.array(dy)
-    dy_ = np.array([np.mean(dy[i:i + 2]) for i in range(len(dy) - 1)])
+    aspect, fwidth = lines[2].split()
+    npts, length = lines[3].split()
 
-    # calculate the positions, assume top of hillslope is 0 y
-    y = [0]
-    for i in range(len(dy) - 1):
-        step = x[i + 1] - x[i]
-        y.append(y[-1] - step * dy_[i])
-    y = np.array(y)
+    fwidth = float(fwidth)
+    length = float(length)
+    area = fwidth * length
 
-    assert len(dy) == len(y), '%i, %i, %i' % (len(x), len(dy), len(y))
-    assert dy.shape == y.shape, '%i, %i' % (dy.shape, y.shape)
+    if length > clip_length:
+        length = clip_length
+        fwidth = area / length
 
-    xi_k = np.repeat(x, 2)
-    yi_k = np.ravel(np.dstack((y, -1 * dy)))
+    lines[2] = f'{aspect} {fwidth}\n'
+    lines[3] = f'{npts} {length}\n'
 
-    #
-    # Return the model
-    #
-    return KroghInterpolator(xi_k, yi_k)
+    with open(dst_fn, 'w') as fp:
+        fp.writelines(lines)
+
+
+def mofe_distance_fractions(fname):
+    with open(fname) as fp:
+        lines = fp.readlines()
+        lines = [L for L in lines if not L.startswith('#')]
+
+    n_ofes = int(lines[1])
+
+    lengths = [0.0]
+    tot_length = 0.0
+    for i in range(n_ofes):
+        ofe_def = lines[3 + i * 2]
+        npts, length = ofe_def.split()
+        length = float(length)
+        lengths.append(length)
+        tot_length += length
+
+    return np.cumsum(lengths) / tot_length
 
 
 class SlopeFile(object):
-    def __init__(self, fname):
-        fid = open(fname)
-        lines = fid.readlines()
-        lines = [L for L in lines if not L.startswith('#')]
+    def __init__(self, fname, z0=10000):
+        with open(fname) as fp:
+            lines = fp.readlines()
+            lines = [L for L in lines if not L.startswith('#')]
 
-        assert int(lines[1]) == 1, 'expecting 1 ofe'
+        n_ofes = int(lines[1])
+        assert n_ofes == 1, 'expecting 1 ofe'
 
-        azm, fwidth = [float(x) for x in lines[2].split()]
+        if lines[0].startswith('2023'):
+            azm, fwidth, z0 = [float(x) for x in lines[2].split()]
+        else:
+            azm, fwidth = [float(x) for x in lines[2].split()]
 
         nSegments, length = lines[3].split()
         nSegments = int(nSegments)
@@ -54,44 +72,38 @@ class SlopeFile(object):
             distances.append(row[i * 2])
             slopes.append(row[i * 2 + 1])
 
-        fid.close()
+        distances_m = [d * length for d in distances]
+        relative_elevs = [z0]
+        for i in range(1, nSegments):
+            dx = distances_m[i] - distances_m[i - 1]
+            relative_elevs.append(relative_elevs[-1] + dx * slopes[i - 1])
 
         self.fname = fname
-        self.hillslope_model = polycurve(distances, slopes)
         self.length = length
+        self.resolution = np.min(np.diff(distances))
         self.nSegments = nSegments
-        self.distances = distances
-        self.slopes = slopes
+        self.distances = np.array(distances)
+        self.slopes = np.array(slopes)
         self.azm = azm
         self.fwidth = fwidth
+        self.relative_elevs = np.array(relative_elevs)
+
+    def interp_slope(self, d):
+        idx = np.searchsorted(self.distances, d)
+        return self.slopes[idx]
+
+    def slope_of_segment(self, d0=0.0, dend=1.0):
+        x0 = d0 * self.length
+        y0 = np.interp(x0, self.distances, self.relative_elevs)
+
+        xend = dend * self.length
+        yend = np.interp(xend, self.distances, self.relative_elevs)
+
+        return (yend - y0) / (xend - x0)
 
     @property
     def slope_scalar(self):
-        x = np.array(self.distances)
-        dy = np.array(self.slopes)
-        dy_ = np.array([np.mean(dy[i:i + 2]) for i in range(len(dy) - 1)])
-
-        # calculate the positions, assume top of hillslope is 0 y
-        y = [0]
-        for i in range(len(dy) - 1):
-            step = x[i + 1] - x[i]
-            y.append(y[-1] - step * dy_[i])
-
-        return -y[-1]
-
-    def _determine_segments(self, d0, dend):
-        i = 0
-        for d in self.distances:
-            if d0 < d < dend:
-                i += 1
-
-        return i + 2
-
-    def _find_points(self, d0, dend, nSegments):
-        distances = np.linspace(d0, dend, nSegments)
-        slopes = self.hillslope_model(distances)
-
-        return distances, slopes
+        return self.slope_of_segment()
 
     def segmented_multiple_ofe(self, 
         dst_fn=None, 
@@ -100,68 +112,62 @@ class SlopeFile(object):
         buffer_length=15,
         min_length=10):
 
-        nSegments = self.nSegments
-        distances = self.distances
-        slopes = self.slopes
         length = self.length
         azm = self.azm
         fwidth = self.fwidth
 
+        d_d = [0.0] # fraction of ofe segment
+        n_mofes = None
         if apply_buffer:
-            r_distances = length * (distances[-1] - np.array(distances[::-1]))
-            buffer_nsegments = np.argmin(np.abs(r_distances - 
-                                                buffer_length)) + 1
-            actual_buffer_length = r_distances[buffer_nsegments-1]
-
-            n_mofes = int(round((length - actual_buffer_length) / target_length))
-            if n_mofes == 0:
-                brks = [0, nSegments-1]
-
-            else:
-                brks = [v for v in np.round(
-                       np.linspace(0, nSegments-buffer_nsegments, n_mofes+1))]
-                brks.append(nSegments-1) 
-                brks = np.array(brks, dtype=int)
+            n_mofes = int(round((length - buffer_length) / target_length)) + 1
+            n_buffer = 1
+            d_buffer = buffer_length / length
+            d_d.append(d_buffer)
 
         else:
             n_mofes = int(round(length / target_length))
-            if n_mofes == 0:
-                n_mofes = 1
+            buffer_length = 0.0
+            n_buffer = 0
+            d_buffer = 0.0
 
-            brks = np.array(np.round(np.linspace(0, nSegments-1, n_mofes+1)), dtype=int) 
-       
+        if n_mofes == 0:
+            n_mofes = 1
 
-        n_mofes = len(brks) - 1
- 
+        ofe_length = (length - buffer_length) / (n_mofes -n_buffer)
+
+        # add non-buffer segments to d_d
+        _d_d = ofe_length / length
+        for i in range(n_mofes - n_buffer):
+            d_d.append(_d_d)
+
+        d_d = np.cumsum(d_d)
+
+        assert d_d[-1] == 1.0, d_d
+        assert len(d_d) == n_mofes + 1, (len(d_d), n_mofes + 1)
+
         s = []
         for i in range(n_mofes):
-            i0 = brks[i]
-            iend = brks[i+1]
+            d0 = d_d[i]
+            dend = d_d[i+1]
 
-            d0 = distances[i0]
-            dend = distances[iend]
-            drange = dend - d0
+            _distance_p = [d0]
+            for _d in self.distances:
+                if d0 < _d < dend:
+                    _distance_p.append(_d)
+            _distance_p.append(dend)
+            _slopes = self.interp_slope(_distance_p)
 
-            slplen = length * drange
+            _npts = len(_slopes)
+            _length = (dend - d0) * length
+            s.append(f'{_npts} {_length:.2f}')
+#            s.append('# ' + ' '.join(f'{_d:.4f}, {_s:.4f}' for _d, _s in zip(_distance_p, _slopes)))
 
-            if min_length is not None:
-                if slplen < min_length:
-                    slplen = min_length
+            _distance_p = (_distance_p - d0) / (dend - d0)
+            s.append('  ' + ' '.join(f'{_d:.4f}, {_s:.4f}' for _d, _s in zip(_distance_p, _slopes)))
 
-            points = iend - i0
-            if points == 0:
-                continue
-
-            s.append(f'{points} {slplen}')
-
-            _distance_p = (np.array(distances[i0:iend]) - d0) / drange
-            _slopes = np.array( slopes[i0:iend])
-            s.append(' '.join(f'{_d}, {_s}' for _d, _s in zip(_distance_p, slopes)))
-
-        n_mofes = int(len(s) / 2)
 
         s = ['97.5',
-             str(n_mofes), 
+             str(n_mofes),
              f'{azm} {fwidth}'] + s
 
         s = '\n'.join(s)
@@ -172,28 +178,3 @@ class SlopeFile(object):
             pf.write(s)
 
         return n_mofes
-
-    def make_multiple_ofe(self, breaks, normalized=True):
-        if not normalized:
-            breaks = breaks/self.length
-
-        for brk in breaks:
-            assert brk > 0.0
-            assert brk < 1.0
-
-        ofes = []
-
-        d0 = 0.0
-        dend = None
-        for brk in breaks:
-            dend = brk
-            nSegments = self._determine_segments(d0, dend)
-            ofes.append(self._find_points(d0, dend, nSegments))
-
-            d0 = brk
-
-        dend = 1.0
-        nSegments = self._determine_segments(d0, dend)
-        ofes.append(self._find_points(d0, dend, nSegments))
-
-
