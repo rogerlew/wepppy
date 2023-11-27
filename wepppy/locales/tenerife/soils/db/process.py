531164bffffa86a10a7abeaff363e6b009ad68e0
Roger Lew <rogerlew@gmail.com>
2023-11-27 10:43:51 -0800

    tenerife soil buildign

--- wepppy/locales/tenerife/soils/db/process.py
+++ wepppy/locales/tenerife/soils/db/process.py
@@ -6,7 +6,7 @@ sols = glob('*.template.sol')
 
 for sol in sols:
 #    print(sol)
-    wsu = WeppSoilUtil(sol, compute_erodibilities=True, compute_conductivity=False)
+    wsu = WeppSoilUtil(sol, compute_erodibilities=True, compute_conductivity=True)
     wsu7778 = wsu.to7778()
     wsu7778.write(sol.replace('.template.sol', '.sol'))
     
