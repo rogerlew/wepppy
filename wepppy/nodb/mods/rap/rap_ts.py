6a098a8b5b19bea0ecd67de03fbd0d568045c589
Roger Lew <rogerlew@gmail.com>
2024-01-22 11:29:00 -0800

    cover transforms

--- wepppy/nodb/mods/rap/rap_ts.py
+++ wepppy/nodb/mods/rap/rap_ts.py
@@ -5,6 +5,7 @@
 #
 # The project described was supported by NSF award number IIA-1301792
 # from the NSF Idaho EPSCoR Program and by the National Science Foundation.
+from typing import Dict, Tuple
 
 import os
 import jsonpickle
@@ -13,6 +14,8 @@ from os.path import join as _join
 from os.path import exists as _exists
 
 from osgeo import gdal
+
+import numpy as np
 
 from wepppy.all_your_base.geo.webclients import wmesque_retrieve
 
@@ -274,6 +277,30 @@ class RAP_TS(NoDbBase, LogMixin):
             yield topaz_id, RAPPointData(**{band.name.lower(): v for band, v in self.data[topaz_id].items()})
 
     def prep_cover(self, runs_dir):
+        from wepppy.nodb.mods.disturbed import Disturbed
+        from wepppy.nodb.mods.revegetation import Revegetation
+
+        wd = self.wd
+
+        disturbed = Disturbed.getInstance(wd)
+        fire_date = None
+        if disturbed is not None:
+            fire_date = disturbed.fire_date
+
+        try:
+            reveg = Revegetation.getInstance(wd)
+        except:
+            reveg = None
+
+        cover_transform = None
+        if reveg is not None:
+            cover_transform = reveg.cover_transform
+
+        if fire_date is not None and  cover_transform is not None:
+            return self._prep_transformed_cover(runs_dir)
+
+
+        self.log('RAP_TS::prep_cover\n')
         data = self.data
 
         watershed = Watershed.getInstance(self.wd)
@@ -291,22 +318,135 @@ class RAP_TS(NoDbBase, LogMixin):
                 fp.write(' \t'.join([str(yr) for yr in years]))
                 fp.write('\n')
 
-
                 if self.multi_ofe:
                     for fp_id in data[RAP_Band.TREE][years[0]][str(topaz_id)]:
+                        self.log(f'  topaz_id={topaz_id}, fp_id={fp_id}\n')
 
                         for band in [RAP_Band.TREE,
                                      RAP_Band.SHRUB,
                                      RAP_Band.PERENNIAL_FORB_AND_GRASS,
-                                     RAP_Band.ANNUAL_FORB_AND_GRASS]:
+                                     RAP_Band.ANNUAL_FORB_AND_GRASS,
+                                     RAP_Band.LITTER,
+                                     RAP_Band.BARE_GROUND,
+                                     ]:
                             fp.write(' \t'.join([str(data[band][yr][topaz_id][fp_id]) for yr in years]))
                             fp.write('\n')
 
                 else:
+                    self.log(f'  topaz_id={topaz_id}\n')
+
                     for band in [RAP_Band.TREE,
                                  RAP_Band.SHRUB,
                                  RAP_Band.PERENNIAL_FORB_AND_GRASS,
-                                 RAP_Band.ANNUAL_FORB_AND_GRASS]:
+                                 RAP_Band.ANNUAL_FORB_AND_GRASS,
+                                 RAP_Band.LITTER,
+                                 RAP_Band.BARE_GROUND,
+                                 ]:
                         fp.write(' \t'.join([str(data[band][yr][str(topaz_id)]) for yr in years]))
+                        fp.write('\n')
+
+    def _prep_transformed_cover(self, runs_dir):
+        from wepppy.nodb.mods.disturbed import Disturbed
+        from wepppy.nodb.mods.revegetation import Revegetation
+        from wepppy.nodb.landuse import Landuse
+
+        self.log('RAP_TS::_prep_transformed_cover\n')
+
+        wd = self.wd
+
+        disturbed = Disturbed.getInstance(wd)
+        reveg = Revegetation.getInstance(wd)
+        landuse = Landuse.getInstance(wd)
+        
+        fire_date = disturbed.fire_date
+        cover_transform = reveg.cover_transform
+        managements = landuse.managements
+
+        data = self.data
+
+        watershed = Watershed.getInstance(self.wd)
+        translator = watershed.translator_factory()
+
+        with open(_join(runs_dir, 'cancov.txt'), 'w') as fp:
+            fp.write("")
+
+        years = [yr for yr in data[RAP_Band.TREE]]
+        fire_mo, fire_da, fire_year = fire_date.replace('-', ' ').replace('/', ' ').split()
+        fire_year = int(fire_year)
+        fire_years = {str(yr): int(yr) - fire_year for yr in years}
+
+        for wepp_id in translator.iter_wepp_sub_ids():
+            topaz_id = str(translator.top(wepp=wepp_id))
+
+            with open(_join(runs_dir, f'p{wepp_id}.cov'), 'w') as fp:
+                fp.write(' \t'.join([str(yr) for yr in years]))
+                fp.write('\n')
+
+                if self.multi_ofe:
+                    for mofe_id in data[RAP_Band.TREE][years[0]][str(topaz_id)]:
+
+                        burn_class = landuse.identify_burn_class(topaz_id, mofe_id)
+
+                        for band, name in [(RAP_Band.TREE, 'Tree'),
+                                    (RAP_Band.SHRUB, 'Shrub'),
+                                    (RAP_Band.PERENNIAL_FORB_AND_GRASS, 'Perennial'),
+                                    (RAP_Band.ANNUAL_FORB_AND_GRASS, 'Annual'),
+                                    (RAP_Band.LITTER, 'Litter'),
+                                    (RAP_Band.BARE_GROUND, 'Bare')
+                                    ]:
+                            key = (burn_class, name)
+
+                            self.log(f'  topaz_id={topaz_id}, mofe_id={mofe_id}, burn_class={burn_class}, rap_band={name}\n')
+                            
+                            if key in cover_transform:
+                                x = []
+                                for yr in years:
+                                    indx = fire_years.get(str(yr), None)
+
+                                    if indx is not None and indx < 0:
+                                        x.append(data[band][yr][topaz_id][mofe_id])
+                                        continue
+
+                                    if indx is None:
+                                        scale = cover_transform[key][-1]
+                                    else:
+                                        scale = cover_transform[key][indx]
+                                    x.append(data[band][str(fire_year)][topaz_id][mofe_id] * scale)
+                                fp.write(' \t'.join([f'{v:0.1f}' for v in x]))
+                            else:
+                                fp.write(' \t'.join([str(data[band][yr][topaz_id][mofe_id]) for yr in years]))
+                            fp.write('\n')
+
+                else:
+                    burn_class = landuse.identify_burn_class(topaz_id)
+
+                    for band, name in [(RAP_Band.TREE, 'Tree'),
+                                 (RAP_Band.SHRUB, 'Shrub'),
+                                 (RAP_Band.PERENNIAL_FORB_AND_GRASS, 'Perennial'),
+                                 (RAP_Band.ANNUAL_FORB_AND_GRASS, 'Annual'),
+                                 (RAP_Band.LITTER, 'Litter'),
+                                 (RAP_Band.BARE_GROUND, 'Bare')
+                                 ]:
+                        key = (burn_class, name)
+                        
+                        self.log(f'  topaz_id={topaz_id}, burn_class={burn_class}, rap_band={name}\n')
+
+                        if key in cover_transform:
+                            x = []
+                            for yr in years:
+                                indx = fire_years.get(str(yr), None)
+
+                                if indx is not None and indx < 0:
+                                    x.append(data[band][yr][topaz_id])
+                                    continue
+
+                                if indx is None:
+                                    scale = cover_transform[key][-1]
+                                else:
+                                    scale = cover_transform[key][indx]
+                                x.append(data[band][str(fire_year)][topaz_id] * scale)
+                            fp.write(' \t'.join([f'{v:0.1f}' for v in x]))
+                        else:
+                            fp.write(' \t'.join([str(data[band][yr][topaz_id]) for yr in years]))
                         fp.write('\n')
 
