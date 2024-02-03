eb079e7271be85a0566053e1853be957609bda86
Roger Lew <rogerlew@gmail.com>
2024-02-02 16:15:14 -0800

    snotel historic daily client

--- wepppy/climates/snotel/__init__.py
+++ wepppy/climates/snotel/__init__.py
@@ -1,15 +1,29 @@
-from zeep import Client
+import requests
 
-client = Client('https://wcc.sc.egov.usda.gov/awdbWebService/services?WSDL')
+target_url = "https://wcc.sc.egov.usda.gov/reportGenerator/view_csv/customSingleStationReport/daily/{}:SNTL%7Cid=%22%22%7Cname/POR_BEGIN,POR_END/WTEQ::value,PREC::value,TMAX::value,TMIN::value,TAVG::value,PRCP::value"
 
-stations = client.service.getStations(logicalAnd=True)
+def download_snotel_data(station_id, output_file):
+    url = target_url.format(station_id.replace('_', ':'))
 
-for station in stations:
-    print(station)
+    response = requests.get(url)
+    csv = response.content.decode('utf-8')
+    with open(output_file, 'w') as f:
+        f.write(csv)
 
-client.service.getHourlyData(
-    stationTriplets='302:OR:SNTL',
-    elementCd='PREC',
-    ordinal=1,
-    beginDate='2000-01-01',
-    endDate='2018-12-31')
+    return response.content
+
+
+if __name__ == "__main__":
+
+    import pandas as pd
+
+    stations = pd.read_csv('/workdir/wepppy/wepppy/climates/validation/snotel_locations.csv')
+    
+    for key, row in stations.iterrows():
+        snotel_lat = row.lat
+        snotel_lng = row.long
+        snotel_id = row.SNTL_stations
+
+        print(snotel_id)
+
+        download_snotel_data(snotel_id, f'historic/{snotel_id}.csv')
