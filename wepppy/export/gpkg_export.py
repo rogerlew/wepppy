744ac45f28c40d0a8b6f4e8f8a5e69d59c91eea3
Roger Lew <rogerlew@gmail.com>
2024-12-09 17:32:22 -0800

    gpkg_export difference map

--- wepppy/export/gpkg_export.py
+++ wepppy/export/gpkg_export.py
@@ -1,5 +1,8 @@
 import geopandas as gpd
 import pandas as pd
+import numpy as np
+
+import json
 
 import os
 from os.path import join as _join
@@ -29,11 +32,6 @@ def esri_compatible_colnames(df):
 
 
 def gpkg_export(wd: str):
-    wat_hill_fn = _join(wd, 'watershed/hillslopes.csv')
-
-    # check if pre peridot
-    if not _exists(wat_hill_fn):
-        return None
 
     if wd.endswith('/'):
         runid = os.path.basename(os.path.dirname(wd))
@@ -48,10 +46,12 @@ def gpkg_export(wd: str):
 
     hill_gdf = gpd.read_file(_join(wd, 'dem/topaz/SUBCATCHMENTS.WGS.JSON'))
     hill_gdf.set_crs("EPSG:4326", inplace=True)
-
-    wat_hill_df = pd.read_csv(wat_hill_fn)
-    wat_hill_df = esri_compatible_colnames(wat_hill_df)
-    hill_gdf = hill_gdf.merge(wat_hill_df, left_on='TopazID', right_on='topaz_id', how='left')
+    
+    wat_hill_fn = _join(wd, 'watershed/hillslopes.csv')
+    if _exists(wat_hill_fn):
+        wat_hill_df = pd.read_csv(wat_hill_fn)
+        wat_hill_df = esri_compatible_colnames(wat_hill_df)
+        hill_gdf = hill_gdf.merge(wat_hill_df, left_on='TopazID', right_on='topaz_id', how='left')
 
     lc_hill_fn = _join(wd, 'landuse/landuse.parquet')
     if _exists(lc_hill_fn):
@@ -122,12 +122,13 @@ def gpkg_export(wd: str):
     chn_gdf.set_crs("EPSG:4326", inplace=True)
 
     wat_chn_fn = _join(wd, 'watershed/channels.csv')
-    wat_chn_df = pd.read_csv(wat_chn_fn)
-    wat_chn_df = esri_compatible_colnames(wat_chn_df)
-    columns_to_drop = ['WeppID']
-    columns_to_drop = [c for c in columns_to_drop if c in wat_chn_df.columns]
-    wat_chn_df.drop(columns=columns_to_drop, inplace=True)
-    chn_gdf = chn_gdf.merge(wat_chn_df, left_on='TopazID', right_on='topaz_id', how='left')
+    if _exists(wat_chn_fn):
+        wat_chn_df = pd.read_csv(wat_chn_fn)
+        wat_chn_df = esri_compatible_colnames(wat_chn_df)
+        columns_to_drop = ['WeppID']
+        columns_to_drop = [c for c in columns_to_drop if c in wat_chn_df.columns]
+        wat_chn_df.drop(columns=columns_to_drop, inplace=True)
+        chn_gdf = chn_gdf.merge(wat_chn_df, left_on='TopazID', right_on='topaz_id', how='left')
 
     chn_loss_fn = _join(wd, 'wepp/output/loss_pw0.chn.parquet')
     if _exists(chn_loss_fn):
@@ -167,7 +168,76 @@ def gpkg_export(wd: str):
     if f_esri.has_f_esri():
         f_esri.gpkg_to_gdb(gpkg_fn, gpkg_fn.replace('.gpkg', '.gdb'))
 
+from pandas.core.series import Series
+
+def create_difference_map(scenario1_gpkg_fn, scenario2_gpkg_fn, difference_attributes, output_geojson_fn,  meta_attributes=None):
+    layer_name = "subcatchments"
+    scenario1_gdf = gpd.read_file(scenario1_gpkg_fn, layer=layer_name)
+    scenario2_gdf = gpd.read_file(scenario2_gpkg_fn, layer=layer_name)
+
+    assert len(scenario1_gdf) == len(scenario2_gdf), "The two maps must have the same number of features."
+    assert scenario1_gdf.crs == scenario2_gdf.crs, "The two maps must have the same CRS."
+
+    features = []
+
+    for feature1, feature2 in zip(scenario1_gdf.iterfeatures(), scenario2_gdf.iterfeatures()):
+        feature1_props = feature1['properties']
+        feature2_props = feature2['properties']
+        
+        print(feature1_props)
+        
+        topaz_id = feature1_props['TopazID']
+        
+
+        new_feature = {
+            "type": "Feature",
+            "geometry": feature1['geometry'],
+            "properties": {}
+        }
+        
+        new_feature["properties"]["topaz_id"] = topaz_id
+        
+        if meta_attributes is not None:
+            for attr in meta_attributes:
+                new_feature["properties"][attr] = feature1_props.get(attr)
+
+        for attr in difference_attributes:
+            value1 = feature1_props.get(attr, np.nan)
+            value2 = feature2_props.get(attr, np.nan)
+            
+            difference = np.nan if np.isnan(value1) or np.isnan(value2) else value1 - value2
+            new_feature["properties"][f"difference_{attr}"] = difference
+
+        features.append(new_feature)
+
+    geojson_output = {
+        "type": "FeatureCollection",
+        "features": features
+    }
+
+    with open(output_geojson_fn, 'w') as f:
+        json.dump(geojson_output, f, indent=2)
+
 
 if __name__ == '__main__':
-    gpkg_export('/geodata/weppcloud_runs/bacterial-anorexia')
+#    gpkg_export('/geodata/weppcloud_runs/six-serine/')
 
+    # Carmen's burned and modified scenarios
+    if 0:
+        create_difference_map('/geodata/weppcloud_runs/six-serine/export/arcmap/six-serine.gpkg', 
+                            '/geodata/weppcloud_runs/hedonic-disqualification/export/arcmap/hedonic-disqualification.gpkg', 
+                            difference_attributes=['Soil_Loss_kg', 'Runoff_mm'],
+                            output_geojson_fn='test/difference_map.geojson')
+    
+    
+    create_difference_map('/geodata/weppcloud_runs/worldwide-trombone/export/arcmap/worldwide-trombone.gpkg', 
+                          '/geodata/weppcloud_runs/multiple-principal/export/arcmap/multiple-principal.gpkg', 
+                          difference_attributes=['Soil_Loss_kg', 'Runoff_mm'],
+                          meta_attributes=['disturbed_class', 'simple_texture'],
+                          output_geojson_fn='/geodata/weppcloud_runs/multiple-principal/export/arcmap/pinegulch_post_difference_map.geojson')
+
+    create_difference_map('/geodata/weppcloud_runs/rlew-dependable-linchpin/export/arcmap/rlew-dependable-linchpin.gpkg', 
+                          '/geodata/weppcloud_runs/rlew-miserable-morphology/export/arcmap/rlew-miserable-morphology.gpkg', 
+                          difference_attributes=['Soil_Loss_kg', 'Runoff_mm'],
+                          meta_attributes=['disturbed_class', 'simple_texture'],
+                          output_geojson_fn='/geodata/weppcloud_runs/rlew-miserable-morphology/export/arcmap/rlew_pinegulch_post_difference_map.geojson')
