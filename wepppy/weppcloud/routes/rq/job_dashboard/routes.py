ddb018e3cb7ff6c3b21a88459e533a870a401046
Roger Lew <rogerlew@gmail.com>
2025-02-19 10:15:23 -0800

    rq-dashboard insert hyphens

--- wepppy/weppcloud/routes/rq/job_dashboard/routes.py
+++ wepppy/weppcloud/routes/rq/job_dashboard/routes.py
@@ -12,6 +12,10 @@ rq_job_dashboard_bp = Blueprint('rq_job_dashboard', __name__, template_folder='t
 
 @rq_job_dashboard_bp.route('/rq/job-dashboard/<string:job_id>')
 def job_dashboard_route(job_id):
+    # job id is a uuid; if it doesn't contain '-' then insert them
+    if len(job_id) == 32:
+        job_id = f'{job_id[:8]}-{job_id[8:12]}-{job_id[12:16]}-{job_id[16:20]}-{job_id[20:]}'
+        
     # Assuming you have a function to get job details
     return render_template('dashboard.html', job_id=job_id)
 
