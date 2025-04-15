2654ecb30d5d6f9f650ee76b0760f72bdbd64cf0
Roger Lew <rogerlew@gmail.com>
2025-04-15 01:03:16 -0700

    chile update

--- wepppy/nodb/soils.py
+++ wepppy/nodb/soils.py
@@ -385,7 +385,7 @@ class Soils(NoDbBase, LogMixin):
                 mukey = domsoil_d.get(str(topaz_id), None)
 
                 if mukey is None:
-                    mukey = domsoil_d[str(topaz_id)] = 1
+                    mukey = domsoil_d[str(topaz_id)] = '1'
 
                 src_sol_fn, soil_id = get_soil_fn(mukey)
                 sol_fn = _join(soils_dir, f'{soil_id}.sol')
