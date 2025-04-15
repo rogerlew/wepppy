557a5e55d115350d0c62ad399787d7a1aae8d6c9
Roger Lew <rogerlew@gmail.com>
2025-04-15 00:49:52 -0700

    chile update

--- wepppy/nodb/soils.py
+++ wepppy/nodb/soils.py
@@ -381,7 +381,9 @@ class Soils(NoDbBase, LogMixin):
             self.log(f'domsoil_d: {repr(domsoil_d)}')
 
             soils = {}
-            for topaz_id, mukey in domsoil_d.items():
+            for topaz_id in watershed.subs_summary:
+                mukey = domsoil_d.get(str(topaz_id), '1-sand loam-forest')
+
                 src_sol_fn, soil_id = get_soil_fn(mukey)
                 sol_fn = _join(soils_dir, f'{soil_id}.sol')
                 if not _exists(sol_fn):
