7767dc570a55cb6ff7ad36373d360aeedfe7ffc5
Roger Lew <rogerlew@gmail.com>
2025-04-17 00:57:32 -0700

    update conda env file

--- install/conda/install-libgfortran3.sh
+++
@@ -1,9 +0,0 @@
-  # !usr/bin/bash
-  echo "Downloading gcc-6-base" && \
-  cd /tmp/ && wget http://archive.ubuntu.com/ubuntu/pool/universe/g/gcc-6/gcc-6-base_6.4.0-17ubuntu1_amd64.deb && \
-  echo "Downloading libgfortran3" && \ 
-  cd /tmp/ && wget http://archive.ubuntu.com/ubuntu/pool/universe/g/gcc-6/libgfortran3_6.4.0-17ubuntu1_amd64.deb && \
-  echo "Installing gcc-6-base" && \
-  cd /tmp/ && sudo dpkg -i gcc-6-base_6.4.0-17ubuntu1_amd64.deb && \ 
-  echo "Installing libgfortran3" && \
-  cd /tmp/ && sudo dpkg -i libgfortran3_6.4.0-17ubuntu1_amd64.deb
