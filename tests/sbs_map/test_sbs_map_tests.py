c8c85c1c25bd266406322fc3aedb0ac11ee67049
Roger Lew <rogerlew@gmail.com>
2025-05-23 09:09:35 -0700

    sbs map sanity checking, reject float maps without integer values

--- tests/sbs_map/test_sbs_map_tests.py
+++ tests/sbs_map/test_sbs_map_tests.py
@@ -1,7 +1,7 @@
 import unittest
 import os
 
-from wepppy.nodb.mods.baer.sbs_map import SoilBurnSeverityMap
+from wepppy.nodb.mods.baer.sbs_map import SoilBurnSeverityMap, sbs_map_sanity_check
 
 _thisdir = os.path.dirname(os.path.abspath(__file__))
 _datadir = os.path.join(_thisdir, 'data')
@@ -11,6 +11,19 @@ class TestImports(unittest.TestCase):
         sbs_fn = '/workdir/wepppy/tests/sbs_map/data/Eaton_SBS_final.tif'
         sbs = SoilBurnSeverityMap(sbs_fn)
         print(sbs.class_pixel_map)
+
+class TestSbsMapSanityCheck(unittest.TestCase):
+    def test_sanity_valid_classes(self):
+        """sbs_map_sanity_check should return 0 code for a valid-class map."""
+        status, msg = sbs_map_sanity_check(os.path.join(_datadir, 'Cayumanque_fire_burn_severity3.tif'))
+        self.assertEqual(status, 0)
+        self.assertEqual(msg, 'Map has valid classes')
+
+    def test_sanity_too_many_classes(self):
+        """sbs_map_sanity_check should flag maps with >256 classes."""
+        status, msg = sbs_map_sanity_check(os.path.join(_datadir, 'SBS.tif'))
+        self.assertEqual(status, 1)
+        self.assertEqual(msg, 'Map has more than 256 classes')
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
