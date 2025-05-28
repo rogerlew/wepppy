1a068a982ad0f4914a8c95735b46a694c5c5506f
Roger Lew <rogerlew@gmail.com>
2025-05-28 10:44:14 -0700

    wepp.out.Element fix for runoff ******** events on first day

---
+++ tests/test_1_wepp_out.py
@@ -0,0 +1,19 @@
+from wepppy.wepp.out import Element
+import unittest
+
+class TestWeppOut(unittest.TestCase):
+    def test_wepp_out_element(self):
+        """
+        validates files with ****** on the frist line are read correctly
+        """
+        element_fn = '/workdir/wepppy/tests/wepp/out/H1.element.dat'
+        ebe = Element(element_fn)
+        self.assertIsInstance(ebe, Element)
+
+    def test_wepp_out_element2(self):
+        """
+        validates files ****** past the first line are read correctly
+        """
+        element_fn = '/workdir/wepppy/tests/wepp/out/H2.element.dat'
+        ebe = Element(element_fn)
+        self.assertIsInstance(ebe, Element)
