ac930410d6972a3cfe9708aa521e5857f11f4121
Roger Lew <rogerlew@gmail.com>
2025-06-09 09:17:01 -0700

    sbs map tests for no ct and 127 nodaa

--- tests/sbs_map/test_sbs_map.py
+++ tests/sbs_map/test_sbs_map.py
@@ -6,11 +6,36 @@ from wepppy.nodb.mods.baer.sbs_map import SoilBurnSeverityMap, sbs_map_sanity_ch
 _thisdir = os.path.dirname(os.path.abspath(__file__))
 _datadir = os.path.join(_thisdir, 'data')
 
+def isint(s):
+    """Check if a string can be converted to an integer."""
+    try:
+        float(int(s)) == float(s)
+        return True
+    except ValueError:
+        return False
+
+
 class TestImports(unittest.TestCase):
     def test_read_no_ct_float64(self):
         sbs_fn = '/workdir/wepppy/tests/sbs_map/data/Eaton_SBS_final.tif'
         sbs = SoilBurnSeverityMap(sbs_fn)
-        print(sbs.class_pixel_map)
+        class_pixel_map = sbs.class_pixel_map
+
+        for k in class_pixel_map:
+            self.assertTrue(isint(k), f"Key {k} in class_pixel_map is not an integer.")
+
+    def test_read_no_ct_127nodata(self):
+        sbs_fn = '/workdir/wepppy/tests/sbs_map/data/Riverside_SBS_final.tif'
+        sbs = SoilBurnSeverityMap(sbs_fn)
+        class_pixel_map = sbs.class_pixel_map
+        self.assertEqual(class_pixel_map['127'], '130')
+
+    def test_sbs_nodata_as_string_guard(self):
+        """Ensure that the nodata value is always a string."""
+        sbs_fn = '/workdir/wepppy/tests/sbs_map/data/Eaton_SBS_final.tif'
+
+        with self.assertRaises(ValueError):
+            SoilBurnSeverityMap(sbs_fn, nodata_vals='[255]')
 
 class TestSbsMapSanityCheck(unittest.TestCase):
     def test_sanity_valid_classes(self):
