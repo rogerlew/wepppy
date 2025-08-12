646aae2ba0bc49d6692215fb2b507a1ded71b349
Roger Lew <rogerlew@gmail.com>
2025-08-12 16:46:41 -0700

    test script for omni contrasts

---
+++ tests/omni/run_contrasts.py
@@ -0,0 +1,28 @@
+
+from wepppy.nodb.omni import Omni, OmniScenario
+
+if __name__ == '__main__':
+    runid = 'rlew-confirmed-complementarity'
+
+    omni = Omni.getInstanceFromRunID(runid)
+    omni.clear_contrasts()
+
+    if not hasattr(omni, '_contrast_names'):
+        omni._contrast_names = None
+
+    control_scenario_def = dict(type=OmniScenario.SBSmap)
+    contrast_scenario_defs = [
+        dict(type=OmniScenario.Mulch, ground_cover_increase='60%', base_scenario=OmniScenario.SBSmap),
+        dict(type=OmniScenario.Mulch, ground_cover_increase='30%', base_scenario=OmniScenario.SBSmap),
+        dict(type=OmniScenario.Mulch, ground_cover_increase='15%', base_scenario=OmniScenario.SBSmap)
+    ]
+
+    for contrast_scenario_def in contrast_scenario_defs:
+        omni.build_contrasts(control_scenario_def, contrast_scenario_def,
+                            obj_param='Soil_Loss_kg',
+                            contrast_cumulative_obj_param_threshold_fraction=0.8)
+        
+    from pprint import pprint
+    pprint(omni.contrasts)
+
+    omni.run_omni_contrasts()
