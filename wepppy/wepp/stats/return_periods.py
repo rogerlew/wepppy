2a261670a43769d7b1ec793999017b83253a4176
Roger Lew <rogerlew@gmail.com>
2025-08-21 01:14:03 -0700

    return periods Hill Sed Del in tonne

--- wepppy/wepp/stats/return_periods.py
+++ wepppy/wepp/stats/return_periods.py
@@ -48,7 +48,7 @@ class ReturnPeriods:
 
         if totwatsed2 is not None:
             wsarea_m2 = totwatsed2.wsarea
-            df['Hill Sed Del'] = totwatsed2.d['Sed Del (kg)']  # kg
+            df['Hill Sed Del'] = totwatsed2.d['Sed Del (kg)'] / 1000.0  # tonne
             df['Hill Streamflow'] = totwatsed2.d['Streamflow (mm)'] / 1000.0 * wsarea_m2  # m^3/s
 
         _years = sorted(set(df['year']))
@@ -131,7 +131,7 @@ class ReturnPeriods:
         self.units_d['Peak Discharge'] = 'm^3/s'
         self.units_d['Sediment Yield'] = 'tonne'
         self.units_d['Storm Duration'] = 'hours'
-        self.units_d['Hill Sed Del'] = 'kg'
+        self.units_d['Hill Sed Del'] = 'tonne'
         self.units_d['Hill Streamflow'] = 'm^3/s'
 
     def to_dict(self):
