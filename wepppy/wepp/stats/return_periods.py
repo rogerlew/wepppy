b12aca63ce3fd4ee24c49f60f6629e1be48e9559
Roger Lew <rogerlew@gmail.com>
2025-08-21 01:29:24 -0700

    Return Periods Hill Streamflow in m^3

--- wepppy/wepp/stats/return_periods.py
+++ wepppy/wepp/stats/return_periods.py
@@ -49,7 +49,7 @@ class ReturnPeriods:
         if totwatsed2 is not None:
             wsarea_m2 = totwatsed2.wsarea
             df['Hill Sed Del'] = totwatsed2.d['Sed Del (kg)'] / 1000.0  # tonne
-            df['Hill Streamflow'] = totwatsed2.d['Streamflow (mm)'] / 1000.0 * wsarea_m2  # m^3/s
+            df['Hill Streamflow'] = totwatsed2.d['Streamflow (mm)'] / 1000.0 * wsarea_m2  # m^3
 
         _years = sorted(set(df['year']))
         _y0 = _years[0]
@@ -132,7 +132,7 @@ class ReturnPeriods:
         self.units_d['Sediment Yield'] = 'tonne'
         self.units_d['Storm Duration'] = 'hours'
         self.units_d['Hill Sed Del'] = 'tonne'
-        self.units_d['Hill Streamflow'] = 'm^3/s'
+        self.units_d['Hill Streamflow'] = 'm^3'
 
     def to_dict(self):
         return {
