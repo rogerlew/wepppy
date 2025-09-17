225902fa2a26226337f06a0e03440c6f206db63e
Roger Lew <rogerlew@gmail.com>
2025-09-17 12:03:04 -0700

    webservice/rq-dashboard

---
+++ wepppy/webservices/rq_dashboard.py
@@ -0,0 +1,13 @@
+# /workdir/wepppy/run_rq_dashboard.py
+
+import dotenv
+from flask import Flask
+from rq_dashboard import blueprint
+import os
+
+app = Flask(__name__)
+app.config["RQ_DASHBOARD_REDIS_URL"] = os.environ.get("RQ_DASHBOARD_REDIS_URL", "redis://129.101.202.237:6379/9")
+app.register_blueprint(blueprint, url_prefix="/")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8040, debug=True)
