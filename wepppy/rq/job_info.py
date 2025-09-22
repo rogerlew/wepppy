d6ff10941b5d08c757e3f0f1c1cfd93232adbedb
Roger Lew <rogerlew@gmail.com>
2025-09-22 09:48:41 -0700

    add rq/api/jobstatus route to rq blueprint

--- wepppy/rq/job_info.py
+++ wepppy/rq/job_info.py
@@ -2,6 +2,7 @@ import os
 from rq import Queue, Worker
 from rq.job import Job
 from rq.utils import get_version
+from rq.exceptions import NoSuchJobError
 import redis
 import json
 
@@ -48,15 +49,38 @@ def get_job_details(job, redis_conn, now):
     return job_info
 
 
-def get_run_wepp_rq_job_info(job_id: str) -> dict:
+def get_wepppy_rq_job_info(job_id: str) -> dict:
     now = utcnow()
     with redis.Redis(host=REDIS_HOST, port=6379, db=RQ_DB) as redis_conn:
-        job = Job.fetch(job_id, connection=redis_conn)
+        try:
+            job = Job.fetch(job_id, connection=redis_conn)
+        except NoSuchJobError:
+            return {"id": job_id, "status": "not_found"}
 
         if not job:
-            return {"error": "Job not found"}
+            return {"id": job_id, "status": "not_found"}
 
         return get_job_details(job, redis_conn, now)
+
+
+def get_wepppy_rq_job_status(job_id: str) -> dict:
+    with redis.Redis(host=REDIS_HOST, port=6379, db=RQ_DB) as redis_conn:
+        try:
+            job = Job.fetch(job_id, connection=redis_conn)
+        except NoSuchJobError:
+            return {"id": job_id, "status": "not_found"}
+
+        if not job:
+            return {"id": job_id, "status": "not_found"}
+
+        return {
+            "id": job.id,
+            "runid": job.meta.get("runid"),
+            "status": job.get_status(),
+            "started_at": str(job.started_at) if job.started_at else None,
+            "ended_at": str(job.ended_at) if job.ended_at else None,
+        }
+
 
 if __name__ == "__main__":
     import sys
@@ -64,6 +88,5 @@ if __name__ == "__main__":
 
     if not sys.argv[-1].endswith('.py'):
         job_id = str(sys.argv[-1])
-        job_info = get_run_wepp_rq_job_info(job_id)
+        job_info = get_wepppy_rq_job_info(job_id)
         print(json.dumps(job_info, indent=2))
-
--- wepppy/weppcloud/routes/rq/api/jobinfo.py
+++ wepppy/weppcloud/routes/rq/api/jobinfo.py
@@ -8,18 +8,41 @@ from os.path import exists as _exists
 from flask import abort, Blueprint, request, jsonify
 from utils.helpers import get_wd, exception_factory, success_factory
 
-from wepppy.rq.job_info import get_run_wepp_rq_job_info
+from wepppy.rq.job_info import get_wepppy_rq_job_info, get_wepppy_rq_job_status
 from wepppy.rq.cancel_job import cancel_jobs
 
 
 rq_jobinfo_bp = Blueprint('rq_jobinfo', __name__)
+
+## Job Status
+# The status of a job can be one of the following:
+#
+# queued: 
+#   The default status for created jobs, except for those that have dependencies, 
+#   which will be created as deferred. These jobs have been placed in a queue and are ready to be executed.
+
+# finished: 
+#   The job has finished execution and is available through the finished job registry.
+# failed: 
+#   Jobs that encountered errors during execution or expired before being executed.
+# started: 
+#   The job has started execution. This status includes the job execution support mechanisms, such as setting the worker name and setting up heartbeat information.
+# deferred: 
+#   The job is not ready for execution because its dependencies have not finished successfully yet.
+# scheduled: 
+#   Jobs created to run at a future date or jobs that are retried after a retry interval.
+# stopped: 
+#   The job was stopped because the worker was stopped.
+# canceled: 
+#   The job has been manually canceled and will not be executed, even if it is part of a dependency chain.
+
 
 
 @rq_jobinfo_bp.route('/rq/api/jobinfo/<string:job_id>')
 @rq_jobinfo_bp.route('/rq/jobinfo/<string:job_id>')
 def jobinfo_route(job_id):
     try:
-        job_info = get_run_wepp_rq_job_info(job_id)
+        job_info = get_wepppy_rq_job_info(job_id)
         return jsonify(job_info)
     except Exception:
         return exception_factory()
@@ -34,3 +57,12 @@ def canceljob_route(job_id):
     except Exception:
         return exception_factory()
 
+
+@rq_jobinfo_bp.route('/rq/api/jobstatus/<string:job_id>')
+@rq_jobinfo_bp.route('/rq/jobstatus/<string:job_id>')
+def jobstatus_route(job_id):
+    try:
+        job_status = get_wepppy_rq_job_status(job_id)
+        return jsonify(job_status)
+    except Exception:
+        return exception_factory()
