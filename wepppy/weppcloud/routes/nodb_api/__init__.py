2f382a653b9ac6d825fe35d39bc7197b112ad594
Roger Lew <rogerlew@gmail.com>
2025-09-26 21:21:44 -0700

    reorg nodb_api to provide distinct filenames from nodb/*

---
+++ wepppy/weppcloud/routes/nodb_api/__init__.py
@@ -0,0 +1,31 @@
+from .climate_bp import climate_bp
+from .debris_flow_bp import debris_flow_bp
+from .disturbed_bp import disturbed_bp
+from .landuse_bp import landuse_bp
+from .observed_bp import observed_bp
+from .omni_bp import omni_bp
+from .rangeland_bp import rangeland_bp
+from .rangeland_cover_bp import rangeland_cover_bp
+from .rhem_bp import rhem_bp
+from .treatments_bp import treatments_bp
+from .unitizer_bp import unitizer_bp
+from .watar_bp import watar_bp
+from .watershed_bp import watershed_bp
+from .wepp_bp import wepp_bp
+
+__all__ = [
+    'climate_bp',
+    'debris_flow_bp',
+    'disturbed_bp',
+    'landuse_bp',
+    'observed_bp',
+    'omni_bp',
+    'rangeland_bp',
+    'rangeland_cover_bp',
+    'rhem_bp',
+    'treatments_bp',
+    'unitizer_bp',
+    'watar_bp',
+    'watershed_bp',
+    'wepp_bp'
+]
