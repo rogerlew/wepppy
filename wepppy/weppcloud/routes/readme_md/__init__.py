58c57be6158d67b46369a1ceb60de2929b17c876
Roger Lew <rogerlew@gmail.com>
2025-09-28 09:07:21 -0700

    fix: include ensure_readme_on_create in imports and __all__ declaration

--- wepppy/weppcloud/routes/readme_md/__init__.py
+++ wepppy/weppcloud/routes/readme_md/__init__.py
@@ -1,4 +1,5 @@
-from .readme_md import readme_bp, ensure_readme
+from .readme_md import readme_bp, ensure_readme, ensure_readme_on_create
 
 __all__ = ["readme_bp", 
-           "ensure_readme"]
+           "ensure_readme",
+           "ensure_readme_on_create"]
