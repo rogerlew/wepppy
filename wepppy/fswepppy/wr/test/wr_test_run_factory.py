4501fed97e31ae5e7ac6a9ff90132cff4cf33ba3
Roger Lew <rogerlew@gmail.com>
2025-10-12 08:22:07 -0700

    rm fswepppy / fswepp2 api replaced this

--- wepppy/fswepppy/wr/test/wr_test_run_factory.py
+++
@@ -1,6 +0,0 @@
-from pprint import pprint
-
-from wepppy.fswepppy.wr import run_factory
-
-d = run_factory('ib n h 7 100 30 17 150 32 101 7 clay https://dev.wepp.cloud/weppcloud/runs/base-monad/disturbed9002/browse/climate/id106388.cli', 'wd')
-pprint(d)
