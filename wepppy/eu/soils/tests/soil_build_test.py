ac3f0f9e8020356bdc870e1310355c9c28faa2a0
Roger Lew <rogerlew@gmail.com>
2025-10-31 11:27:28 +0000

    Adjust ESDAC soil build test for optional dependencies

--- wepppy/eu/soils/tests/soil_build_test.py
+++ wepppy/eu/soils/tests/soil_build_test.py
@@ -1,5 +1,23 @@
-from wepppy.eu.soils.esdac import ESDAC
+import os
 
-esd = ESDAC()
-esd.build_wepp_soil(-6.309, 43.140013)
+import pytest
 
+ESDAC_RASTER_DIR = "/geodata/eu/ESDAC_ESDB_rasters"
+ESDAC_STU_DIR = "/geodata/eu/ESDAC_STU_EU_Layers"
+
+pytestmark = pytest.mark.skipif(
+    not (os.path.isdir(ESDAC_RASTER_DIR) and os.path.isdir(ESDAC_STU_DIR)),
+    reason="ESDAC rasters unavailable",
+)
+
+
+def collection_error(tmp_path):
+    pytest.importorskip("numpy")
+    pytest.importorskip("requests")
+    from wepppy.eu.soils.esdac import ESDAC
+
+    esd = ESDAC()
+    esd.build_wepp_soil(-6.309, 43.140013, str(tmp_path))
+
+
+test_collection_error = collection_error
