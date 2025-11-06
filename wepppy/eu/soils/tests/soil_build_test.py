11fc1f28a1a388c9d3b878309beb79c56721ee7c
Roger Lew <rogerlew@gmail.com>
2025-11-06 11:15:48 +0000

    Skip EU Soil HydroGrids-dependent test when data missing

--- wepppy/eu/soils/tests/soil_build_test.py
+++ wepppy/eu/soils/tests/soil_build_test.py
@@ -4,10 +4,15 @@ import pytest
 
 ESDAC_RASTER_DIR = "/geodata/eu/ESDAC_ESDB_rasters"
 ESDAC_STU_DIR = "/geodata/eu/ESDAC_STU_EU_Layers"
+EU_SOIL_HYDROGRIDS_SAMPLE = "/geodata/eu/eusoilhydrogrids/KS_sl1/KS_sl1.tif"
 
 pytestmark = pytest.mark.skipif(
-    not (os.path.isdir(ESDAC_RASTER_DIR) and os.path.isdir(ESDAC_STU_DIR)),
-    reason="ESDAC rasters unavailable",
+    not (
+        os.path.isdir(ESDAC_RASTER_DIR)
+        and os.path.isdir(ESDAC_STU_DIR)
+        and os.path.exists(EU_SOIL_HYDROGRIDS_SAMPLE)
+    ),
+    reason="ESDAC rasters or EU Soil HydroGrids data unavailable",
 )
 
 
