6a0c416c9845a0bd155abb1199bfc19997a2fc80
Roger Lew <rogerlew@gmail.com>
2025-12-23 09:06:15 -0800

    update stubs

--- wepppy/nodb/core/__init__.pyi
+++ wepppy/nodb/core/__init__.pyi
@@ -1,10 +1,10 @@
 from .climate import Climate as Climate, ClimateMode as ClimateMode, ClimateModeIsUndefinedError as ClimateModeIsUndefinedError, ClimateSpatialMode as ClimateSpatialMode, NoClimateStationSelectedError as NoClimateStationSelectedError
 from .landuse import Landuse as Landuse, LanduseMode as LanduseMode
-from .ron import Ron as Ron
+from .ron import Map as Map, Ron as Ron
 from .soils import Soils as Soils, SoilsMode as SoilsMode
 from .topaz import Topaz as Topaz
 from wepppy.topo.topaz import MinimumChannelLengthTooShortError as MinimumChannelLengthTooShortError, WatershedBoundaryTouchesEdgeError as WatershedBoundaryTouchesEdgeError
 from .watershed import Watershed as Watershed, WatershedNotAbstractedError as WatershedNotAbstractedError
 from .wepp import Wepp as Wepp
 
-__all__ = ['Climate', 'ClimateMode', 'ClimateSpatialMode', 'NoClimateStationSelectedError', 'ClimateModeIsUndefinedError', 'Landuse', 'LanduseMode', 'Ron', 'Soils', 'SoilsMode', 'Topaz', 'WatershedBoundaryTouchesEdgeError', 'MinimumChannelLengthTooShortError', 'Watershed', 'WatershedNotAbstractedError', 'Wepp']
+__all__ = ['Climate', 'ClimateMode', 'ClimateSpatialMode', 'NoClimateStationSelectedError', 'ClimateModeIsUndefinedError', 'Landuse', 'LanduseMode', 'Map', 'Ron', 'Soils', 'SoilsMode', 'Topaz', 'WatershedBoundaryTouchesEdgeError', 'MinimumChannelLengthTooShortError', 'Watershed', 'WatershedNotAbstractedError', 'Wepp']
